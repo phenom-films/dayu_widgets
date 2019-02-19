47cb5655b5e3fd105d3c21d79a7dd5cd219e18d9
muyanru <muyanru@phenom-films.com>
2019-02-19 17:06:17 +0800

    add qt module

---
+++ dayu_widgets/qt/__init__.py
@@ -0,0 +1,44 @@
+try:
+    from PySide2.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtWidgets import *
+except:
+    from PySide.QtCore import *
+    from PySide.QtGui import *
+    from PySide.QtWebKit import *
+
+
+class MWidget(QWidget):
+    def __init__(self, parent=None):
+        super(MWidget, self).__init__(parent)
+
+    def addProperty(self, attr, value=None):
+        self.setProperty(attr, value)
+
+    def event(self, event):
+        if event.type() == QEvent.DynamicPropertyChange:
+            p = event.propertyName()
+            if hasattr(self, 'set_{}'.format(p)):
+                callback = getattr(self, 'set_{}'.format(p))
+                callback(self.property(str(p)))
+        return super(QWidget, self).event(event)
+
+
+class MPixmapCacheDict(object):
+    def __init__(self, cls):
+        super(MPixmapCacheDict, self).__init__()
+        self.cls = cls
+        self._cache_pix_dict = {}
+
+    def __call__(self, path):
+        assert isinstance(path, basestring)
+        lower_path = unicode(path.lower())
+        pix_map = self._cache_pix_dict.get(lower_path, None)
+        if pix_map is None:
+            pix_map = self.cls(lower_path)
+            self._cache_pix_dict.update({lower_path: pix_map})
+        return pix_map
+
+
+MPixmap = MPixmapCacheDict(QPixmap)
+MIcon = MPixmapCacheDict(QIcon)
