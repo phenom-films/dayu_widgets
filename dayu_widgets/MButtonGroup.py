418a45d193e2d8e1f2cac4562b514d0ce214d13a
muyanru <muyanru@phenom-films.com>
2019-02-20 21:54:41 +0800

    add property; change qss

--- dayu_widgets/MButtonGroup.py
+++ dayu_widgets/MButtonGroup.py
@@ -7,18 +7,6 @@
 ###################################################################
 
 from qt import *
-from MTheme import global_theme
-
-qss = '''
-MButtonGroup[orientation=horizontal] QPushButton {{
-    border-radius: 0;
-    border-left: 1px solid {border};
-}}
-MButtonGroup[orientation=vertical] QPushButton {{
-    border-radius: 0;
-    border-top: 1px solid {border};
-}}
-'''.format(**global_theme)
 
 
 @property_mixin
@@ -33,8 +21,7 @@ class MButtonGroup(QWidget):
         self.setLayout(self._main_layout)
         self.setProperty('type', 'MButtonGroup')
         self.setProperty('orientation', 'horizontal' if orientation == Qt.Horizontal else 'vertical')
-        self.setStyleSheet(qss)
 
     def add_button(self, button):
-        button.setStyleSheet(button.styleSheet() + qss)
+        button.setProperty('combine', self.property('orientation'))
         self._main_layout.insertWidget(self._main_layout.count() - 1, button)
