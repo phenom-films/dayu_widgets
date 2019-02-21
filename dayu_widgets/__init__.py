08d64c339da8f1928e653af29fdf300afa0d024c
muyanru <muyanru@phenom-films.com>
2019-02-21 20:36:43 +0800

    add static_folder

---
+++ dayu_widgets/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+STATIC_FOLDERS = [os.path.join(__path__[0], 'static')]
+
+from MButton import MButton
+from MButtonGroup import MButtonGroup
+from MRadioGroup import MRadioGroup
+from MDivider import MDivider
