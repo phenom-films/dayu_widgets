b3f83a4fd91a5ab31193d550bd2c6a785e72e66d
muyanru <muyanru@phenom-films.com>
2019-02-21 16:39:36 +0800

    change qss

--- dayu_widgets/MLabel.py
+++ dayu_widgets/MLabel.py
@@ -6,11 +6,13 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from qt import *
 from MTheme import global_theme
+from qt import *
 
 qss = '''
-QLabel
+QLabel{{
+    padding: 2px;
+}}
 QLabel[type=main_head]{{
     {main_head_font}
     {font_family}
@@ -31,10 +33,18 @@ QLabel[type=help]{{
     {help_font}
     {font_family}
 }}
-QLabel[type=link]{{
-    {link_font}
-    {font_family}
+
+QLabel[link=true]{{
+    color: {primary};
 }}
+QLabel[link=true]:hover{{
+    color: {primary_light};
+}}
+QLabel[link=true]:pressed{{
+    color: {primary_dark};
+}}
+
+
 QLabel:disabled{{
     {disabled_font}
     {font_family}
@@ -42,16 +52,26 @@ QLabel:disabled{{
 '''.format(**global_theme)
 
 
+@property_mixin
 class MLabel(QLabel):
+    '''
+    自定义 props:
+        type: enum
+        link: bool
+    '''
     MainHeadType = 'main_head'
     SubHeadType = 'sub_head'
     SmallHeadType = 'small_head'
     TextType = 'text'
     HelpType = 'help'
-    LinkType = 'like'
 
-    def __init__(self, text='', type=None, parent=None):
-        super(MLabel, self).__init__(parent=parent)
-        self.setProperty('text', text)
+    def __init__(self, text='', type=None, link=False, parent=None, flags=0):
+        super(MLabel, self).__init__(text, parent, flags)
         self.setProperty('type', type or MLabel.TextType)
+        self.setTextInteractionFlags(Qt.TextBrowserInteraction | Qt.LinksAccessibleByMouse)
         self.setStyleSheet(qss)
+        self.setProperty('link', link)
+
+    def set_link(self, value):
+        self.setCursor(Qt.PointingHandCursor if value else Qt.ArrowCursor)
+        self.style().polish(self)
