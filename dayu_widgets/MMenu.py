12a8bb77a656f9624d5053f5727655eb91f2e076
muyanru <muyanru@phenom-films.com>
2019-03-01 17:57:46 +0800

    add MMenu

---
+++ dayu_widgets/MMenu.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+import functools
+from MTheme import global_theme
+from MButton import MButton
+from MLabel import MLabel
+from MAvatar import MAvatar
+import utils
+from qt import *
+
+qss = '''
+QMenu {{
+    {text_font}
+    background-color: {background};
+    padding: 0;
+    border-radius: 2px;
+    border: 1px solid {border};
+}} 
+QMenu[checked=true]{{
+    color: {primary};
+    background-color: {background_selected};
+}}
+QMenu::item {{
+    padding: 2px 2px;
+}}
+QMenu::indicator {{
+    left: 6px;
+}}
+QMenu::item {{
+    padding: 8px auto;
+}}
+QMenu::item:checked  {{
+    color: {primary};
+    background-color: {background_selected};
+}}
+QMenu::item:selected  {{
+    background-color: {background_selected};
+}}
+'''.format(**global_theme)
+
+
+def from_list_to_nested_dict(input, sep='/'):
+    result = []
+    for item in input:
+        components = item.strip(sep).split(sep)
+        component_count = len(components)
+        current = result
+        for i, c in enumerate(components):
+            atom = next((x for x in current if x['value'] == c), None)
+            if atom is None:
+                atom = {'value': c, 'label': c, 'children': []}
+                current.append(atom)
+            current = atom['children']
+            if i == component_count - 1:
+                atom.pop('children')
+    return result
+
+
+@property_mixin
+class MMenu(QMenu):
+    sig_value_changed = Signal(list)
+    sig_index_changed = Signal(int)
+
+    def __init__(self, exclusive=True, cascader=False, title='', parent=None):
+        super(MMenu, self).__init__(title=title, parent=parent)
+        self.setProperty('cascader', cascader)
+        self.setCursor(Qt.PointingHandCursor)
+        self._action_group = QActionGroup(self)
+        self._action_group.setExclusive(exclusive)
+        self._action_group.triggered.connect(self.slot_on_action_triggered)
+        self._load_data_func = None
+        self.set_value('')
+        self.set_separator('/')
+        self.setStyleSheet(qss)
+
+    def set_separator(self, chr):
+        self.setProperty('separator', chr)
+
+    def set_load_callback(self, func):
+        assert callable(func)
+        self._load_data_func = func
+        self.aboutToShow.connect(self.slot_fetch_data)
+
+    def slot_fetch_data(self):
+        data_list = self._load_data_func()
+        self.set_data(data_list)
+
+    def set_value(self, data):
+        print type(data)
+        assert isinstance(data, (basestring, list, int))
+        if isinstance(data, int):
+            action = self._action_group.actions()[data]
+            data = action.property('value')
+        elif self.property('cascader') and isinstance(data, basestring):
+            data = data.split(self.property('separator'))
+        self.setProperty('value', data)
+
+    def _set_value(self, value):
+        data_list = value if isinstance(value, list) else [value]
+        flag = False
+        for act in self._action_group.actions():
+            checked = act.property('value') in data_list
+            if act.isChecked() != checked:  # 更新来自代码
+                act.setChecked(checked)
+                flag = True
+        if flag:
+            self.sig_value_changed.emit(value)
+
+    def set_cascader_data(self, option_list):
+        assert isinstance(option_list, list)
+        assert all(isinstance(i, basestring) for i in option_list) or all(isinstance(i, dict) for i in option_list)
+        if all(isinstance(i, basestring) for i in option_list):
+            option_list = from_list_to_nested_dict(option_list, sep=self.property('separator'))
+        self.setProperty('cascader_data', option_list)
+
+    def _add_menu(self, parent_menu, data_dict):
+        if 'children' in data_dict:
+            menu = QMenu(title=data_dict.get('label'), parent=self)
+            menu.setProperty('value', data_dict.get('value'))
+            parent_menu.addMenu(menu)
+            if not (parent_menu is self):
+                menu.setProperty('parent_menu', parent_menu)
+            for i in data_dict.get('children'):
+                self._add_menu(menu, i)
+        else:
+            action = self._action_group.addAction(utils.default_formatter(data_dict.get('label')))
+            action.setProperty('value', data_dict.get('value'))
+            action.setCheckable(True)
+            action.setProperty('parent_menu', parent_menu)
+            parent_menu.addAction(action)
+
+    def _set_cascader_data(self, option_list):
+        for data_dict in option_list:
+            self._add_menu(self, data_dict)
+
+    def set_data(self, option_list):
+        assert isinstance(option_list, list)
+        option_list = [{'label': i, 'value': i} if isinstance(i, basestring) else i for i in option_list]
+        self.setProperty('data', option_list)
+
+    def _set_data(self, option_list):
+        self.clear()
+        for act in self._action_group.actions():
+            self._action_group.removeAction(act)
+
+        for item in option_list:
+            action = self._action_group.addAction(utils.default_formatter(item.get('label')))
+            action.setProperty('value', item.get('value'))
+            action.setCheckable(True)
+        self.addActions(self._action_group.actions())
+
+    def _get_parent(self, result, obj):
+        if obj.property('parent_menu'):
+            parent_menu = obj.property('parent_menu')
+            result.insert(0, parent_menu.title())
+            self._get_parent(result, parent_menu)
+
+    @Slot(QAction)
+    def slot_on_action_triggered(self, action):
+        current_data = action.property('value')
+        if self.property('cascader'):
+            selected_data = [current_data]
+            self._get_parent(selected_data, action)
+        else:
+            if self._action_group.isExclusive():
+                selected_data = current_data
+            else:
+                selected_data = [act.property('value') for act in self._action_group.actions() if act.isChecked()]
+        self.set_value(selected_data)
+        self.sig_value_changed.emit(selected_data)
+        self.sig_index_changed.emit(self._action_group.actions().index(action))
+
+    def set_loader(self, func):
+        self._load_data_func = func
---
+++ dayu_widgets/examples/MMenuTest.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.qt import *
+from dayu_widgets.MButton import MButton
+from dayu_widgets.MLabel import MLabel
+from dayu_widgets.MMenu import MMenu
+from dayu_widgets.MDivider import MDivider
+from dayu_widgets.MFieldMixin import MFieldMixin
+
+
+class MMenuTest(QWidget, MFieldMixin):
+    def __init__(self, parent=None):
+        super(MMenuTest, self).__init__(parent)
+        self._init_ui()
+
+    def _init_ui(self):
+        self.register_field('button1_selected', u'北京')
+        self.register_field('button1_selected_text', lambda: self.field('button1_selected'))
+        button1 = MButton(text='Normal Menu', type=MButton.PrimaryType)
+        menu1 = MMenu()
+        menu1.set_data([u'北京', u'上海', u'广州', u'深圳'])
+        button1.setMenu(menu1)
+        button1.clicked.connect(button1.showMenu)
+        label1 = MLabel()
+
+        self.bind('button1_selected', menu1, 'value', signal='sig_value_changed')
+        self.bind('button1_selected_text', label1, 'text')
+
+        self.register_field('button2_selected', [u'北京'])
+        self.register_field('button2_selected_text', lambda: ', '.join(self.field('button2_selected')))
+        button2 = MButton(text='Multi Select Menu', type=MButton.PrimaryType)
+        menu2 = MMenu(exclusive=False)
+        menu2.set_data([u'北京', u'上海', u'广州', u'深圳'])
+        button2.setMenu(menu2)
+        button2.clicked.connect(button2.showMenu)
+        label2 = MLabel()
+        self.bind('button2_selected', menu2, 'value', signal='sig_value_changed')
+        self.bind('button2_selected_text', label2, 'text')
+
+        self.register_field('button3_selected', '')
+        self.register_field('button3_selected_text', lambda: self.field('button3_selected'))
+        button3 = MButton(text=u'回调函数获取选项', type=MButton.PrimaryType)
+        menu3 = MMenu()
+        menu3.set_load_callback(lambda: [u'北京', u'上海', u'广州', u'深圳'])
+        button3.setMenu(menu3)
+        button3.clicked.connect(button2.showMenu)
+        label3 = MLabel()
+        self.bind('button3_selected', menu3, 'value', signal='sig_value_changed')
+        self.bind('button3_selected_text', label3, 'text')
+
+        self.register_field('button4_selected', '')
+        self.register_field('button4_selected_text', lambda: ' / '.join(self.field('button4_selected')))
+        button4 = MButton(text=u'级联选择', type=MButton.PrimaryType)
+        menu4 = MMenu(cascader=True)
+        menu4.set_cascader_data([u'北京/故宫', u'北京/天坛', u'北京/王府井', u'江苏/南京/夫子庙', u'江苏/苏州/拙政园', u'江苏/苏州/狮子林'])
+        button4.setMenu(menu4)
+        button4.clicked.connect(button4.showMenu)
+        label4 = MLabel()
+        self.bind('button4_selected', menu4, 'value', signal='sig_value_changed')
+        self.bind('button4_selected_text', label4, 'text')
+
+        a = [{'children': [{'value': u'\u6545\u5bab', 'label': u'\u6545\u5bab'},
+                           {'value': u'\u5929\u575b', 'label': u'\u5929\u575b'},
+                           {'value': u'\u738b\u5e9c\u4e95', 'label': u'\u738b\u5e9c\u4e95'}],
+              'value': u'\u5317\u4eac',
+              'label': u'\u5317\u4eac'},
+             {'children': [{'children': [{'value': u'\u592b\u5b50\u5e99', 'label': u'\u592b\u5b50\u5e99'}],
+                            'value': u'\u5357\u4eac',
+                            'label': u'\u5357\u4eac'},
+                           {'children': [{'value': u'\u62d9\u653f\u56ed', 'label': u'\u62d9\u653f\u56ed'},
+                                         {'value': u'\u72ee\u5b50\u6797', 'label': u'\u72ee\u5b50\u6797'}],
+                            'value': u'\u82cf\u5dde',
+                            'label': u'\u82cf\u5dde'}],
+              'value': u'\u6c5f\u82cf',
+              'label': u'\u6c5f\u82cf'}]
+
+        sub_lay1 = QHBoxLayout()
+        sub_lay1.addWidget(button1)
+        sub_lay1.addWidget(label1)
+        sub_lay2 = QHBoxLayout()
+        sub_lay2.addWidget(button2)
+        sub_lay2.addWidget(label2)
+        sub_lay3 = QHBoxLayout()
+        sub_lay3.addWidget(button3)
+        sub_lay3.addWidget(label3)
+        sub_lay4 = QHBoxLayout()
+        sub_lay4.addWidget(button4)
+        sub_lay4.addWidget(label4)
+
+        main_lay = QVBoxLayout()
+        main_lay.addWidget(MDivider(u'Select'))
+        main_lay.addLayout(sub_lay1)
+        main_lay.addLayout(sub_lay2)
+        main_lay.addLayout(sub_lay3)
+        main_lay.addWidget(MDivider(u'级联选择'))
+        main_lay.addLayout(sub_lay4)
+
+        self.setLayout(main_lay)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    test = MMenuTest()
+    test.show()
+    sys.exit(app.exec_())
