fd478346cfc6fecbb436bad58cc0e046c1e304a1
muyanru <muyanru@phenom-films.com>
2019-03-09 16:09:34 +0800

    refactor render svg

--- dayu_widgets/qt/__init__.py
+++ dayu_widgets/qt/__init__.py
@@ -37,29 +37,37 @@ class MCacheDict(object):
         self.cls = cls
         self._cache_pix_dict = {}
 
+    def _render_svg(self, svg_path, replace_color=None):
+        if (self.cls is QIcon) and (replace_color is None):
+            return QIcon(svg_path)
+        with open(svg_path, 'r+') as f:
+            data_content = f.read()
+            if replace_color is not None:
+                data_content = data_content.replace('#555555', replace_color)
+            self._render.load(QByteArray(data_content))
+            pix = QPixmap(128, 128)
+            pix.fill(Qt.transparent)
+            painter = QPainter(pix)
+            self._render.render(painter)
+            painter.end()
+            if self.cls is QPixmap:
+                return pix
+            else:
+                return self.cls(pix)
+
     def __call__(self, path, color=None):
         assert isinstance(path, basestring)
         import os
         from dayu_widgets import STATIC_FOLDERS
-        path = next((os.path.join(prefix, path) for prefix in [''] + STATIC_FOLDERS if
-                     os.path.isfile(os.path.join(prefix, path))), path)
-        key = unicode(path.lower() + (color or ''))
+        full_path = next((os.path.join(prefix, path) for prefix in [''] + STATIC_FOLDERS if
+                          os.path.isfile(os.path.join(prefix, path))), path)
+        key = unicode(full_path.lower() + (color or ''))
         pix_map = self._cache_pix_dict.get(key, None)
         if pix_map is None:
-            if path.endswith('svg') and isinstance(color, basestring):
-                with open(path, 'r+') as f:
-                    self._render.load(QByteArray(f.read().replace('#555555', color)))
-                    pix = QPixmap(128, 128)
-                    pix.fill(Qt.transparent)
-                    painter = QPainter(pix)
-                    self._render.render(painter)
-                    painter.end()
-                    if self.cls is QIcon:
-                        pix_map = QIcon(pix)
-                    elif self.cls is QPixmap:
-                        pix_map = pix
+            if full_path.endswith('svg'):
+                pix_map = self._render_svg(full_path, color)
             else:
-                pix_map = self.cls(path)
+                pix_map = self.cls(full_path)
             self._cache_pix_dict.update({key: pix_map})
         return pix_map
 
