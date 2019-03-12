eae81688a28ea51d667407bf0591677c3b291167
muyanru <muyanru@phenom-films.com>
2019-03-12 19:36:38 +0800

    add MSwitch

---
+++ dayu_widgets/MSwitch.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+from MTheme import global_theme
+from . import STATIC_FOLDERS
+from qt import *
+
+qss = '''
+QRadioButton{{
+    spacing: -20px;
+
+}}
+QRadioButton::indicator{{
+    subcontrol-origin: border;
+    subcontrol-position: center left;
+    image: url(sphere.svg);
+}}
+QRadioButton[line_size=large]::indicator{{
+    width: 48px;
+    height: 24px;
+    border-radius: 12px;
+}}
+QRadioButton[line_size=default]::indicator{{
+    width: 38px;
+    height: 19px;
+    border-radius: 9px;
+}}
+QRadioButton[line_size=small]::indicator{{
+    width: 28px;
+    height: 14px;
+    border-radius: 7px;
+}}
+QRadioButton::indicator:checked{{
+    image-position: center right;
+    background-color: {primary};
+}}
+QRadioButton::indicator:unchecked{{
+    image-position: center left;
+    background-color: {background_dark};
+}}
+QRadioButton::indicator:disabled{{
+    background-color: {disabled};
+}}
+'''.format(**global_theme)
+qss = qss.replace('url(', 'url({}/'.format(STATIC_FOLDERS[0].replace('\\', '/')))
+
+
+@property_mixin
+class MSwitch(QWidget):
+    '''
+    自定义 props:
+        checked
+    '''
+    sig_checked_changed = Signal(bool)
+
+    def __init__(self, size=None, parent=None):
+        super(MSwitch, self).__init__(parent)
+        self.setProperty('line_size', size or MView.DefaultSize)
+        self.radio = QRadioButton()
+        self.radio.setProperty('line_size', size or MView.DefaultSize)
+        self.radio.toggled.connect(self.set_checked)
+        self.radio.toggled.connect(self.sig_checked_changed)
+        main_lay = QVBoxLayout()
+        main_lay.setContentsMargins(0, 0, 0, 0)
+        main_lay.addWidget(self.radio)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setLayout(main_lay)
+        self.setStyleSheet(qss)
+
+    def minimumSizeHint(self, *args, **kwargs):
+        height = global_theme.get(self.property('line_size') + '_size') * 1.2
+        return QSize(height, height / 2)
+
+    def _set_checked(self, value):
+        if value != self.radio.isChecked():
+            # 更新来自代码
+            self.radio.setChecked(value)
+            self.sig_checked_changed.emit(value)
+        self.style().polish(self)
+
+    def set_checked(self, flag):
+        self.setProperty('checked', flag)
+
+    def enterEvent(self, *args, **kwargs):
+        QApplication.setOverrideCursor(Qt.PointingHandCursor if self.isEnabled() else Qt.ForbiddenCursor)
+        return super(MSwitch, self).enterEvent(*args, **kwargs)
+
+    def leaveEvent(self, *args, **kwargs):
+        QApplication.restoreOverrideCursor()
+        return super(MSwitch, self).leaveEvent(*args, **kwargs)
---
+++ dayu_widgets/examples/MSwitchTest.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.qt import *
+from dayu_widgets.MSwitch import MSwitch
+from dayu_widgets.MDivider import MDivider
+from dayu_widgets.MFieldMixin import MFieldMixin
+from dayu_widgets.MLabel import MLabel
+from dayu_widgets.MButton import MButton
+
+
+class MSwitchTest(QWidget, MFieldMixin):
+    def __init__(self, parent=None):
+        super(MSwitchTest, self).__init__(parent)
+        self._init_ui()
+
+    def _init_ui(self):
+        check_box_1 = MSwitch()
+        check_box_1.set_checked(True)
+        check_box_2 = MSwitch()
+        check_box_3 = MSwitch()
+        check_box_3.setEnabled(False)
+        lay = QHBoxLayout()
+        lay.addWidget(check_box_1)
+        lay.addWidget(check_box_2)
+        lay.addWidget(check_box_3)
+
+        check_box_large = MSwitch(size=MView.LargeSize)
+        check_box_large.set_checked(True)
+        check_box_default = MSwitch()
+        check_box_small = MSwitch(size=MView.SmallSize)
+        lay2 = QHBoxLayout()
+        lay2.addWidget(check_box_large)
+        lay2.addWidget(check_box_default)
+        lay2.addWidget(check_box_small)
+        # check_box_icon_1 = MSwitch('Folder')
+        # check_box_icon_1.setIcon(MIcon(''))
+        #
+        # check_box_b = MSwitch('Data Bind')
+        # label = MLabel()
+        # button = MButton(text='Change State')
+        # button.clicked.connect(lambda :self.set_field('checked', not self.field('checked')))
+        # self.register_field('checked', True)
+        # self.register_field('checked_text', lambda :'Yes!' if self.field('checked') else 'No!!')
+        # self.bind('checked', check_box_b, 'checked', signal='stateChanged')
+        # self.bind('checked_text', label, 'text')
+
+
+        main_lay = QVBoxLayout()
+        main_lay.addWidget(MDivider('Basic'))
+        main_lay.addLayout(lay)
+        main_lay.addWidget(MDivider('size'))
+        main_lay.addLayout(lay2)
+        # main_lay.addWidget(check_box_icon_1)
+        # main_lay.addWidget(MDivider('Data Bind'))
+        # main_lay.addWidget(check_box_b)
+        # main_lay.addWidget(label)
+        # main_lay.addWidget(button)
+        main_lay.addStretch()
+        self.setLayout(main_lay)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    test = MSwitchTest()
+    test.show()
+    sys.exit(app.exec_())
