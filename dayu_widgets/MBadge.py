cbe340c5039197f70d608eaf105cfbf7ca5aef8e
muyanru <muyanru@phenom-films.com>
2019-03-19 19:14:04 +0800

    add MBadge

---
+++ dayu_widgets/MBadge.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.MLabel import MLabel
+from dayu_widgets.MTheme import global_theme
+from dayu_widgets.mixin import property_mixin
+from dayu_widgets.qt import *
+
+qss = '''
+QWidget{{
+    margin: 0;
+}}
+QPushButton{{
+    font-size:10px;
+    min-height:16px;
+    max-height:16px;
+    min-width:16px;
+    border-radius: 8px;
+    background-color: red;
+    border: none;
+    color: white;
+    font-weight:bold;
+    margin: 0;
+}}
+
+QPushButton[dot=true]{{
+    min-height:8px;
+    max-height:8px;
+    min-width:8px;
+    max-width:8px;
+    border-radius: 4px;
+    padding: 0 0;
+}}
+
+'''.format(**global_theme)
+
+
+@property_mixin
+class MBadge(QWidget):
+    '''
+    props:
+        text: basestring
+        count: basestring
+    '''
+
+    def __init__(self, widget, dot=False, count=None, text=None, overflow_count=99, parent=None):
+        super(MBadge, self).__init__(parent)
+        self._widget = widget
+        self.overflow_count = overflow_count
+        self._badge_button = QPushButton()
+        self._badge_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        self._main_lay = QGridLayout()
+        self._main_lay.setContentsMargins(0, 0, 0, 0)
+        self._main_lay.addWidget(widget, 0, 0)
+        self._main_lay.addWidget(self._badge_button, 0, 0, Qt.AlignTop | Qt.AlignRight)
+        self.setLayout(self._main_lay)
+        self.setAttribute(Qt.WA_StyledBackground)
+        self.setStyleSheet(qss)
+        if text is not None:
+            self.set_text(text)
+        if count is not None:
+            self.set_count(count)
+
+        self.set_dot(dot)
+
+    def _set_text(self, value):
+        if not self._badge_button.property('dot'):
+            self._badge_button.setText(value)
+
+    def set_text(self, value):
+        self.setProperty('text', value)
+
+    def _set_dot(self, value):
+        self._badge_button.setVisible(value)
+        self._badge_button.setProperty('dot', value)
+
+    def set_dot(self, value):
+        self.setProperty('dot', value)
+
+    def _set_count(self, value):
+        self._badge_button.setVisible(value > 0)
+        if not self._badge_button.property('dot'):
+            self._badge_button.setText(
+                str(value) if value <= self.overflow_count else '{}+'.format(self.overflow_count))
+
+    def set_count(self, value):
+        self.setProperty('count', value)
---
+++ dayu_widgets/examples/MBadgeTest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.qt import *
+from dayu_widgets.MToolButton import MToolButton
+from dayu_widgets.MAvatar import MAvatar
+from dayu_widgets.MBadge import MBadge
+from dayu_widgets.MDivider import MDivider
+from dayu_widgets.MComboBox import MComboBox
+from dayu_widgets.MPushButton import MPushButton
+from dayu_widgets.MSpinBox import MSpinBox
+from dayu_widgets.MLabel import MLabel
+from dayu_widgets.MMenu import MMenu
+from dayu_widgets.MFieldMixin import MFieldMixin
+
+
+class MBadgeTest(QWidget, MFieldMixin):
+    def __init__(self, parent=None):
+        super(MBadgeTest, self).__init__(parent)
+        self._init_ui()
+
+    def _init_ui(self):
+        button = MToolButton(icon=MIcon('trash_line.svg'))
+        avatar = MAvatar(size=MView.LargeSize, image=MPixmap('avatar.png'))
+        button_alert = MToolButton(icon=MIcon('alert_fill.svg'), size=MView.LargeSize)
+        badge_1 = MBadge(widget=button, dot=True)
+        badge_2 = MBadge(widget=avatar, dot=True)
+        badge_3 = MBadge(widget=button_alert)
+        button.clicked.connect(lambda :badge_1.set_dot(False))
+
+        spin_box = MSpinBox()
+        spin_box.setRange(0, 9999)
+        spin_box.valueChanged.connect(lambda x:badge_3.set_count(x))
+        spin_box.setValue(1)
+
+        self.register_field('button1_selected', u'北京')
+        menu1 = MMenu()
+        menu1.set_data([u'北京', u'上海', u'广州', u'深圳'])
+        select1 = MComboBox()
+        select1.set_menu(menu1)
+        self.bind('button1_selected', select1, 'value', signal='sig_value_changed')
+
+        badge_hot = MBadge(widget=MLabel(u'你的理想城市'), text='hot')
+
+        sub_lay1 = QHBoxLayout()
+        sub_lay1.addWidget(badge_1)
+        sub_lay1.addWidget(badge_2)
+        sub_lay1.addWidget(badge_3)
+
+        sub_lay2 = QHBoxLayout()
+        sub_lay2.addWidget(badge_hot)
+        sub_lay2.addWidget(select1)
+        sub_lay2.addStretch()
+
+        main_lay = QVBoxLayout()
+        main_lay.addWidget(MDivider('different type'))
+        main_lay.addLayout(sub_lay1)
+        main_lay.addWidget(spin_box)
+        main_lay.addWidget(MDivider('different type'))
+        main_lay.addLayout(sub_lay2)
+        main_lay.addStretch()
+        self.setLayout(main_lay)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    test = MBadgeTest()
+    test.show()
+    sys.exit(app.exec_())
