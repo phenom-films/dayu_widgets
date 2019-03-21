f945dc8a19c777d70e513aed6bbb895dcc66d8d6
muyanru <muyanru@phenom-films.com>
2019-03-21 22:12:20 +0800

    use new qss

--- dayu_widgets/MCheckBox.py
+++ dayu_widgets/MCheckBox.py
@@ -6,62 +6,15 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from dayu_widgets import STATIC_FOLDERS
-from dayu_widgets.MTheme import global_theme
-from dayu_widgets.mixin import property_mixin, cursor_mixin
+from dayu_widgets.mixin import property_mixin, cursor_mixin, theme_mixin
 from dayu_widgets.qt import *
-
-qss = '''
-QCheckBox {{
-    spacing: 4px;
-    {text_font}
-    {font_family}
-}}
-QCheckBox:disabled {{
-    color: {disabled};
-}}
-
-QCheckBox::indicator{{
-    width: 13px;
-    height: 13px;
-    border-radius: 2px;
-    border: 1px solid {border};
-    background-color: white;
-}}
-QCheckBox::indicator:disabled{{
-    border: 1px solid {border};
-    background-color: {background_selected};
-}}
-
-QCheckBox::indicator:hover{{
-    border: 1px solid {primary_light};
-    background-color: white;
-}}
-
-QCheckBox::indicator:checked{{
-    background-color: {primary};
-    image: url(check.svg);
-}}
-
-QCheckBox::indicator:checked:disabled{{
-    background-color: {disabled};
-}}
-
-QCheckBox::indicator:indeterminate {{
-    background-color: {primary};
-    image: url(minus.svg);
-}}
-
-QCheckBox::indicator:indeterminate:disabled {{
-    background-color: {disabled};
-}}
-'''.format(**global_theme)
-qss = qss.replace('url(', 'url({}/'.format(STATIC_FOLDERS[0].replace('\\', '/')))
 
 
 @property_mixin
 @cursor_mixin
+@theme_mixin
 class MCheckBox(QCheckBox):
-    def __init__(self, text='', parent=None):
+    def __init__(self, text='', icon=None, parent=None):
         super(MCheckBox, self).__init__(text=text, parent=parent)
-        self.setStyleSheet(qss)
+        if icon:
+            self.setIcon(icon)
--- dayu_widgets/examples/MCheckBoxTest.py
+++ dayu_widgets/examples/MCheckBoxTest.py
@@ -11,16 +11,18 @@ from dayu_widgets.MDivider import MDivider
 from dayu_widgets.MFieldMixin import MFieldMixin
 from dayu_widgets.MLabel import MLabel
 from dayu_widgets.MPushButton import MPushButton
+from dayu_widgets.mixin import theme_mixin
 from dayu_widgets.qt import *
 
 
+@theme_mixin
 class MCheckBoxTest(QWidget, MFieldMixin):
     def __init__(self, parent=None):
         super(MCheckBoxTest, self).__init__(parent)
         self._init_ui()
 
     def _init_ui(self):
-        check_box_1 = MCheckBox('Unchecked')
+        check_box_1 = MCheckBox('Unchecked', icon=MIcon('trash_line.svg'))
         check_box_1.setChecked(False)
 
         check_box_2 = MCheckBox('Checked')
@@ -42,26 +44,24 @@ class MCheckBoxTest(QWidget, MFieldMixin):
         check_box_6.setCheckState(Qt.PartiallyChecked)
 
         grid_lay = QGridLayout()
-        grid_lay.addWidget(check_box_1, 0,0)
-        grid_lay.addWidget(check_box_2, 0,1)
-        grid_lay.addWidget(check_box_3, 0,2)
+        grid_lay.addWidget(check_box_1, 0, 0)
+        grid_lay.addWidget(check_box_2, 0, 1)
+        grid_lay.addWidget(check_box_3, 0, 2)
 
-        grid_lay.addWidget(check_box_4, 1,0)
-        grid_lay.addWidget(check_box_5, 1,1)
-        grid_lay.addWidget(check_box_6, 1,2)
+        grid_lay.addWidget(check_box_4, 1, 0)
+        grid_lay.addWidget(check_box_5, 1, 1)
+        grid_lay.addWidget(check_box_6, 1, 2)
 
-        check_box_icon_1 = MCheckBox('Folder')
-        check_box_icon_1.setIcon(MIcon(''))
+        check_box_icon_1 = MCheckBox('Folder', icon=MIcon('folder_line.svg'))
 
         check_box_b = MCheckBox('Data Bind')
         label = MLabel()
         button = MPushButton(text='Change State')
-        button.clicked.connect(lambda :self.set_field('checked', not self.field('checked')))
+        button.clicked.connect(lambda: self.set_field('checked', not self.field('checked')))
         self.register_field('checked', True)
-        self.register_field('checked_text', lambda :'Yes!' if self.field('checked') else 'No!!')
+        self.register_field('checked_text', lambda: 'Yes!' if self.field('checked') else 'No!!')
         self.bind('checked', check_box_b, 'checked', signal='stateChanged')
         self.bind('checked_text', label, 'text')
-
 
         main_lay = QVBoxLayout()
         main_lay.addWidget(MDivider('Basic'))
