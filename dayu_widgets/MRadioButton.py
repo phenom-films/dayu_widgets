569b272b2ba420e0bbf92739191ffc1086041e29
muyanru <muyanru@phenom-films.com>
2019-03-25 16:36:41 +0800

    use new qss

--- dayu_widgets/MRadioButton.py
+++ dayu_widgets/MRadioButton.py
@@ -6,12 +6,11 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from dayu_widgets.mixin import cursor_mixin, theme_mixin
+from dayu_widgets.mixin import cursor_mixin
 from dayu_widgets.qt import *
 
 
 @cursor_mixin
-@theme_mixin
 class MRadioButton(QRadioButton):
     def __init__(self, text='', icon=None, parent=None):
         super(MRadioButton, self).__init__(text=text, parent=parent)
