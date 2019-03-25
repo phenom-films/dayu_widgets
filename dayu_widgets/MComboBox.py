21c4ca1be4af50fcd8db66c28f4e3c0151a48b5e
muyanru <muyanru@phenom-films.com>
2019-03-25 16:12:08 +0800

    use new qss

--- dayu_widgets/MComboBox.py
+++ dayu_widgets/MComboBox.py
@@ -6,59 +6,9 @@
 # Email : muyanru345@163.com
 ###################################################################
 import dayu_widgets.utils as utils
-from dayu_widgets import STATIC_FOLDERS
-from dayu_widgets.MTheme import global_theme
+from dayu_widgets.MTheme import dayu_theme
 from dayu_widgets.mixin import property_mixin, cursor_mixin
 from dayu_widgets.qt import *
-
-qss = '''
-QComboBox{{
-    {text_font}
-    border: 1px solid {border};
-    border-radius: 4px;
-}}
-
-QComboBox::drop-down {{
-    subcontrol-origin: border;
-    subcontrol-position: center right;
-    right: 5px;
-    height: 20px;
-    width: 20px;
-    image: url(down_line.svg);
-}}
-
-QComboBox:focus, QComboBox:hover{{
-    border: 1px solid {primary};
-}}
-
-QComboBox[line_size=default]{{
-    min-height: {default_size}px;
-    max-height: {default_size}px;
-}}
-QComboBox[line_size=large]{{
-    font-size: 16px;
-    border-radius: 4px;
-    min-height: {large_size}px;
-    max-height: {large_size}px;
-}}
-QComboBox[line_size=large]::drop-down{{
-    right: 6px;
-    height: 24px;
-    width: 24px;
-}}
-QComboBox[line_size=small]{{
-    border-radius: 2px;
-    min-height: {small_size}px;
-    max-height: {small_size}px;
-}}
-QComboBox[line_size=small]::drop-down{{
-    right: 4px;
-    height: 18px;
-    width: 18px;
-}}
-'''.format(**global_theme)
-
-qss = qss.replace('url(', 'url({}/'.format(STATIC_FOLDERS[0].replace('\\', '/')))
 
 
 @property_mixin
@@ -69,19 +19,19 @@ class MComboBox(QComboBox):
 
     def __init__(self, size=None, parent=None):
         super(MComboBox, self).__init__(parent)
-        size = size or MView.DefaultSize
+        size = size or dayu_theme.default_size
         self._root_menu = None
         self._display_formatter = utils.default_formatter
-        self.setProperty('line_size', size)
-        self.setStyleSheet(qss)
-        self.setCursor(Qt.PointingHandCursor)
+        self.setProperty('dayu_size', size)
         self.setEditable(True)
         line_edit = self.lineEdit()
         line_edit.setReadOnly(True)
         line_edit.setTextMargins(4, 0, 4, 0)
-        line_edit.setStyleSheet('border-radius: 4px;')
+        line_edit.setProperty('dayu_size', size)
+        line_edit.setStyleSheet('background-color:transparent')
         line_edit.setCursor(Qt.PointingHandCursor)
         line_edit.installEventFilter(self)
+        self._has_custom_view = False
         self.set_value('')
         self.set_placeholder(u'请选择')
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
@@ -105,9 +55,16 @@ class MComboBox(QComboBox):
         self._root_menu.sig_value_changed.connect(self.sig_value_changed)
         self._root_menu.sig_value_changed.connect(self.set_value)
 
+    def setView(self, *args, **kwargs):
+        self._has_custom_view = True
+        super(MComboBox, self).setView(*args, **kwargs)
+
     def showPopup(self):
-        QComboBox.hidePopup(self)
-        self._root_menu.popup(self.mapToGlobal(QPoint(0, self.height())))
+        if self._has_custom_view:
+            super(MComboBox, self).showPopup()
+        else:
+            QComboBox.hidePopup(self)
+            self._root_menu.popup(self.mapToGlobal(QPoint(0, self.height())))
 
     def setCurrentIndex(self, index):
         raise NotImplementedError
