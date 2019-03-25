746cb0f3751c1f139f787e148c7f5f66b9c6310b
muyanru <muyanru@phenom-films.com>
2019-03-25 17:25:32 +0800

    edit detail

--- dayu_widgets/MToolButton.py
+++ dayu_widgets/MToolButton.py
@@ -6,148 +6,43 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from dayu_widgets import STATIC_FOLDERS
-from dayu_widgets.MTheme import global_theme
+from dayu_widgets.MTheme import dayu_theme
 from dayu_widgets.mixin import property_mixin, cursor_mixin
 from dayu_widgets.qt import *
-
-qss = '''
-QToolButton {{
-    border-radius: 2px;
-    padding: 1px;
-    {font_family}
-    {text_font}
-}}
-
-QToolButton[icon_only=false]{{
-    {text_font}
-    {font_family}
-    background-color: {background};
-    border: 1px solid {border};
-    qproperty-toolButtonStyle: ToolButtonTextBesideIcon;
-}}
-QToolButton[icon_only=false]:hover{{
-    color: {primary_light};
-    border-color: {primary_light};
-}}
-QToolButton[icon_only=false]:pressed{{
-    color: {primary_dark};
-    border-color: {primary_dark};
-}}
-QToolButton[icon_only=false]:checked{{
-    color: {primary};
-    border: 2px solid {primary};
-}}
-
-QToolButton[icon_only=true]{{
-    background-color: transparent;
-    qproperty-toolButtonStyle: ToolButtonIconOnly;
-}}
-QToolButton[icon_only=true]:disabled{{
-    background-color: {background_selected};
-}}
-QToolButton[icon_only=true]:pressed{{
-    border-color: {primary_dark};
-}}
-QToolButton[icon_only=true]:hover{{
-    border:1px solid {primary_light};
-}}
-QToolButton[icon_only=true]:checked{{
-    border:1px solid {primary};
-}}
-
-QToolButton[combine=horizontal]{{
-    border-radius: 0;
-}}
-QToolButton[combine=vertical]{{
-    border-radius: 0;
-}}
-
-
-QToolButton[button_size=default]{{
-    min-height: {default_size}px;
-    max-height: {default_size}px;
-    qproperty-iconSize: {default_icon_size}px {default_icon_size}px;
-}}
-QToolButton[button_size=large]{{
-    font-size:16px;
-    min-height: {large_size}px;
-    max-height: {large_size}px;
-    qproperty-iconSize: {large_icon_size}px {large_icon_size}px;
-}}
-QToolButton[button_size=small]{{
-    min-height: {small_size}px;
-    max-height: {small_size}px;
-    qproperty-iconSize: {small_icon_size}px {small_icon_size}px;
-}}
-QToolButton[button_size=tiny]{{
-    min-height: {tiny_size}px;
-    max-height: {tiny_size}px;
-    qproperty-iconSize: {tiny_icon_size}px {tiny_icon_size}px;
-}}
-
-QToolButton[button_size=default][icon_only=true]{{
-    min-width: {default_size}px;
-    max-width: {default_size}px;
-}}
-QToolButton[button_size=large][icon_only=true]{{
-    min-width: {large_size}px;
-    max-width: {large_size}px;
-}}
-QToolButton[button_size=small][icon_only=true]{{
-    min-width: {small_size}px;
-    max-width: {small_size}px;
-}}
-QToolButton[button_size=tiny][icon_only=true]{{
-    min-width: {tiny_size}px;
-    max-width: {tiny_size}px;
-}}
-
-QToolButton::menu-indicator {{
-    subcontrol-origin: border;
-    subcontrol-position: bottom right;
-    right: 5px;
-    bottom: 5px;
-    height: 10px;
-    width: 10px;
-    image: url(down_fill.svg);
-}}
-
-'''.format(**global_theme)
-
-qss = qss.replace('url(', 'url({}/'.format(STATIC_FOLDERS[0].replace('\\', '/')))
 
 
 @property_mixin
 @cursor_mixin
 class MToolButton(QToolButton):
-    def __init__(self, icon=None, icon_checked=None, size=None, checkable=False, icon_only=True, parent=None):
+    NormalType = 'normal'
+    TaoBaoType = 'taobao'
+    IconOnlyType = 'icon'
+    BarType = 'bar'
+    BreadcrumbType = 'breadcrumb'
+
+    def __init__(self, text='', icon=None, icon_checked=None, type=None, size=None, parent=None):
         super(MToolButton, self).__init__(parent=parent)
         self.setProperty('icon_unchecked', icon)
         self.setProperty('icon_checked', icon_checked or icon)
+        self.setAutoExclusive(False)
         self.setAutoRaise(True)
-        self.slot_polish_icon()
-        self.toggled.connect(self.slot_polish_icon)
+        self._polish_icon()
+        self.toggled.connect(self._polish_icon)
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        self.setStyleSheet(qss)
-        self.setCheckable(checkable)
-        self.set_button_size(size or MView.DefaultSize)
-        self.set_icon_only(icon_only)
+        size = size or dayu_theme.default_size
+        self.setProperty('type', type or MToolButton.NormalType)
+        self.setProperty('dayu_size', size)
+        if type == MToolButton.IconOnlyType:
+            self.setFixedSize(QSize(size, size))
+            self.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        else:
+            self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+            self.setText(text)
+        if type == MToolButton.TaoBaoType:
+            self.setCheckable(True)
 
     @Slot(bool)
-    def slot_polish_icon(self, checked=None):
+    def _polish_icon(self, checked=None):
         icon = self.property('icon_checked') if self.isChecked() else self.property('icon_unchecked')
         if icon:
             self.setIcon(icon)
-
-    def _set_icon_only(self, value):
-        self.style().polish(self)
-
-    def set_icon_only(self, value):
-        self.setProperty('icon_only', value)
-
-    def _set_button_size(self, value):
-        self.style().polish(self)
-
-    def set_button_size(self, value):
-        self.setProperty('button_size', value)
--- dayu_widgets/examples/MToolButtonGroupTest.py
+++ dayu_widgets/examples/MToolButtonGroupTest.py
@@ -6,102 +6,96 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-import functools
-
 from dayu_widgets.MButtonGroup import MToolButtonGroup
 from dayu_widgets.MDivider import MDivider
 from dayu_widgets.MFieldMixin import MFieldMixin
-from dayu_widgets.MPushButton import MPushButton
-from dayu_widgets.MTheme import global_theme
+from dayu_widgets.MTheme import dayu_theme
+from dayu_widgets.MToolButton import MToolButton
+from dayu_widgets.mixin import theme_mixin
 from dayu_widgets.qt import *
 
 
+@theme_mixin
 class MToolButtonGroupTest(QWidget, MFieldMixin):
     def __init__(self, parent=None):
         super(MToolButtonGroupTest, self).__init__(parent)
         self._init_ui()
 
     def _init_ui(self):
-        tool_group_h = MToolButtonGroup(icon_only=False, size=MView.SmallSize)
+        tool_group_h = MToolButtonGroup(size=dayu_theme.size.small)
         tool_group_h.set_button_list(['Apple', {'text': 'Banana'}, {'text': 'Pear'}])
+        tool_1_lay = QHBoxLayout()
+        tool_1_lay.addWidget(tool_group_h)
+        tool_1_lay.addStretch()
 
         app_data = [
-            {'text': 'Maya', 'icon': MIcon('app-maya.png')},
-            {'text': 'Nuke', 'icon': MIcon('app-nuke.png')},
-            {'text': 'Houdini', 'icon': MIcon('app-houdini.png')}
+            {'text': 'Maya', 'icon': MIcon('app-maya.png'), 'checkable': True},
+            {'text': 'Nuke', 'icon': MIcon('app-nuke.png'), 'checkable': True},
+            {'text': 'Houdini', 'icon': MIcon('app-houdini.png'), 'checkable': True}
         ]
 
-        tool_group_v = MToolButtonGroup(icon_only=False, size=MView.SmallSize, orientation=Qt.Vertical, checkable=True)
+        tool_group_v = MToolButtonGroup(type=MToolButton.TaoBaoType,
+                                        exclusive=True,
+                                        size=dayu_theme.size.small,
+                                        orientation=Qt.Vertical)
         tool_group_v.set_button_list(app_data)
 
         tool_group_button_h = MToolButtonGroup()
         tool_group_button_h.set_button_list(app_data)
+        tool_2_lay = QHBoxLayout()
+        tool_2_lay.addWidget(tool_group_button_h)
+        tool_2_lay.addStretch()
 
-
-        tool_grp_excl_true = MToolButtonGroup(orientation=Qt.Horizontal, checkable=True)
+        tool_grp_excl_true = MToolButtonGroup(orientation=Qt.Horizontal, exclusive=True, type=MToolButton.IconOnlyType)
         tool_grp_excl_true.set_button_list([
-            {'icon': MIcon('table_view.svg'),
-             'icon_checked': MIcon('table_view.svg', global_theme.get('primary')),
+            {'icon': MIcon('table_view.svg'), 'checkable': True,
+             'icon_checked': MIcon('table_view.svg', dayu_theme.color.get('primary')),
              'tooltip': u'Table View'},
-            {'icon': MIcon('list_view.svg'),
-             'icon_checked': MIcon('list_view.svg', global_theme.get('primary')),
+            {'icon': MIcon('list_view.svg'), 'checkable': True,
+             'icon_checked': MIcon('list_view.svg', dayu_theme.color.get('primary')),
              'tooltip': u'List View'},
-            {'icon': MIcon('tree_view.svg'),
-             'icon_checked': MIcon('tree_view.svg', global_theme.get('primary')),
+            {'icon': MIcon('tree_view.svg'), 'checkable': True,
+             'icon_checked': MIcon('tree_view.svg', dayu_theme.color.get('primary')),
              'tooltip': u'Tree View'},
-            {'icon': MIcon('big_view.svg'),
-             'icon_checked': MIcon('big_view.svg', global_theme.get('primary')),
+            {'icon': MIcon('big_view.svg'), 'checkable': True,
+             'icon_checked': MIcon('big_view.svg', dayu_theme.color.get('primary')),
              'tooltip': u'Big View'},
         ])
         tool_grp_excl_true.set_checked(0)
+        tool_excl_lay = QHBoxLayout()
+        tool_excl_lay.addWidget(tool_grp_excl_true)
+        tool_excl_lay.addStretch()
 
-        tool_grp_excl_false = MToolButtonGroup(orientation=Qt.Horizontal, checkable=True, icon_only=False)
-        tool_grp_excl_false.set_button_list(app_data)
-        # self.register_field('value1', -1)
-        # self.register_field('value1_text', functools.partial(self.value_to_text, 'value1', app_data))
-        # self.register_field('value2', 0)
-        # self.register_field('value2_text', functools.partial(self.value_to_text, 'value2', app_data))
-        # self.register_field('value3', -1)
-        # self.register_field('value3_text', functools.partial(self.value_to_text, 'value3', app_data))
-
-        button1 = MPushButton(text='Group 1', type=MPushButton.PrimaryType)
-        button2 = MPushButton(text='Group 2', type=MPushButton.PrimaryType)
-        button3 = MPushButton(text='Group 3', type=MPushButton.PrimaryType)
-        button1.clicked.connect(functools.partial(self.slot_change_value, 'value1'))
-        button2.clicked.connect(functools.partial(self.slot_change_value, 'value2'))
-        button3.clicked.connect(functools.partial(self.slot_change_value, 'value3'))
-
-        # self.bind('value1', tool_group_v, 'checked', signal='sig_checked_changed')
-        # self.bind('value2', tool_group_button_h, 'checked', signal='sig_checked_changed')
-        # self.bind('value3', tool_group_button_v, 'checked', signal='sig_checked_changed')
-        # self.bind('value1_text', button1, 'text')
-        # self.bind('value2_text', button2, 'text')
-        # self.bind('value3_text', button3, 'text')
+        tool_grp_excl_false = MToolButtonGroup(orientation=Qt.Horizontal,
+                                               type=MToolButton.IconOnlyType,
+                                               exclusive=False)
+        # TODO: 这里表现的不对
+        tool_grp_excl_false.set_button_list(
+            [
+                {'tooltip': u'加粗', 'icon': MIcon('bold.svg'),
+                 'icon_checked': MIcon('bold.svg', dayu_theme.color.get('primary')), 'checkable': True},
+                {'tooltip': u'倾斜', 'icon': MIcon('italic.svg'),
+                 'icon_checked': MIcon('italic.svg', dayu_theme.color.get('primary')), 'checkable': True},
+                {'tooltip': u'下划线', 'icon': MIcon('underline.svg'),
+                 'icon_checked': MIcon('underline.svg', dayu_theme.color.get('primary')), 'checkable': True},
+            ])
+        tool_excl_2_lay = QHBoxLayout()
+        tool_excl_2_lay.addWidget(tool_grp_excl_false)
+        tool_excl_2_lay.addStretch()
 
         main_lay = QVBoxLayout()
         main_lay.addWidget(MDivider('orientation=Qt.Horizontal '))
-        main_lay.addWidget(tool_group_h)
+        main_lay.addLayout(tool_1_lay)
         main_lay.addWidget(MDivider('orientation=Qt.Vertical'))
         main_lay.addWidget(tool_group_v)
-        main_lay.addWidget(button1)
         main_lay.addWidget(MDivider('orientation=Qt.Horizontal'))
-        main_lay.addWidget(tool_group_button_h)
-        main_lay.addWidget(button2)
+        main_lay.addLayout(tool_2_lay)
         main_lay.addWidget(MDivider('checkable=True; exclusive=True'))
-        main_lay.addWidget(tool_grp_excl_true)
+        main_lay.addLayout(tool_excl_lay)
         main_lay.addWidget(MDivider('checkable=True; exclusive=False'))
-        main_lay.addWidget(tool_grp_excl_false)
-        main_lay.addWidget(button3)
+        main_lay.addLayout(tool_excl_2_lay)
         main_lay.addStretch()
         self.setLayout(main_lay)
-
-    def value_to_text(self, field, data_list):
-        return 'Please Select One' if self.field(field) < 0 else \
-            'You Selected [{}]'.format(data_list[self.field(field)].get('text'))
-
-    def slot_change_value(self, attr):
-        import random
-        self.set_field(attr, random.randrange(0, 3))
 
 
 if __name__ == '__main__':
