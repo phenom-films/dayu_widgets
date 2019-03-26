ca3ffca424d983293c01fd678a230e8ad3f88b46
muyanru <muyanru@phenom-films.com>
2019-03-26 18:02:22 +0800

    add MLineTabWidget

---
+++ dayu_widgets/MLineTabWidget.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.3
+# Email : muyanru345@163.com
+###################################################################
+
+
+from dayu_widgets.MToolButton import MToolButton
+from dayu_widgets.MButtonGroup import MButtonGroupBase
+from dayu_widgets.MDivider import MDivider
+from dayu_widgets.qt import *
+from dayu_widgets import dayu_theme
+
+
+class MLineButtonGroup(MButtonGroupBase):
+    sig_checked_changed = Signal(int)
+
+    def __init__(self, size=None, orientation=Qt.Horizontal, parent=None):
+        super(MLineButtonGroup, self).__init__(orientation=orientation, parent=parent)
+        self.set_spacing(1)
+        self._button_group.setExclusive(True)
+        self._size = size
+        self._orientation = orientation
+        self._button_group.buttonClicked[int].connect(self.set_checked)
+        self._button_group.buttonClicked[int].connect(self.sig_checked_changed)
+        self.set_checked(-1)
+
+    def create_button(self, data_dict):
+        button = MToolButton(text=data_dict.get('text'),
+                             icon=data_dict.get('icon'),
+                             type=MToolButton.LineType,
+                             size=data_dict.get('size') or self._size,
+                             parent=self
+                             )
+        button.setCheckable(True)
+        if data_dict.get('icon') or data_dict.get('icon_checked'):
+            button.setProperty('icon_checked', data_dict.get('icon_checked'))
+            button.setProperty('icon_unchecked', data_dict.get('icon'))
+        return button
+
+    def _set_checked(self, value):
+        assert isinstance(value, int)
+        if value != self._button_group.checkedId():
+            # 更新来自代码
+            button = self._button_group.button(value)
+            if button:
+                button.setChecked(True)
+            self.sig_checked_changed.emit(value)
+
+    def set_checked(self, value):
+        self.setProperty('checked', value)
+
+
+class MLineTabWidget(QWidget):
+    def __init__(self, alignment=Qt.AlignCenter, size=None, parent=None):
+        super(MLineTabWidget, self).__init__(parent=parent)
+        self.tool_button_group = MLineButtonGroup(size=size)
+        self.bar_layout = QHBoxLayout()
+        self.bar_layout.setContentsMargins(0, 0, 0, 0)
+        if alignment == Qt.AlignCenter:
+            self.bar_layout.addStretch()
+            self.bar_layout.addWidget(self.tool_button_group)
+            self.bar_layout.addStretch()
+        elif alignment == Qt.AlignLeft:
+            self.bar_layout.addWidget(self.tool_button_group)
+            self.bar_layout.addStretch()
+        elif alignment == Qt.AlignRight:
+            self.bar_layout.addStretch()
+            self.bar_layout.addWidget(self.tool_button_group)
+        self.stack_lay = QStackedLayout()
+        self.tool_button_group.sig_checked_changed.connect(self.stack_lay.setCurrentIndex)
+        main_lay = QVBoxLayout()
+        main_lay.setContentsMargins(0, 0, 0, 0)
+        main_lay.setSpacing(0)
+        main_lay.addLayout(self.bar_layout)
+        main_lay.addWidget(MDivider())
+        main_lay.addSpacing(5)
+        main_lay.addLayout(self.stack_lay)
+        self.setLayout(main_lay)
+
+    def add_tab(self, widget, data_dict):
+        self.stack_lay.addWidget(widget)
+        self.tool_button_group.add_button(data_dict, self.stack_lay.count() - 1)
---
+++ dayu_widgets/examples/MLineTabWidgetTest.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.3
+# Email : muyanru345@163.com
+###################################################################
+from dayu_widgets.MDivider import MDivider
+from dayu_widgets.MLabel import MLabel
+from dayu_widgets.MMessage import MMessage
+from dayu_widgets.MLineTabWidget import MLineTabWidget
+from dayu_widgets.qt import *
+
+
+class MLineTabWidgetTest(QWidget):
+    def __init__(self, parent=None):
+        super(MLineTabWidgetTest, self).__init__(parent)
+        self._init_ui()
+
+    def _init_ui(self):
+        main_lay = QVBoxLayout()
+
+        tab_center = MLineTabWidget()
+        tab_center.add_tab(MLabel('test 1'), u'Current Element')
+        tab_center.add_tab(MLabel('test 2'), u'Linked Assets')
+        tab_center.add_tab(MLabel('test 3'), u'Hero Shots')
+        tab_center.add_tab(MLabel('test 4'), u'Linked Metadata')
+        tab_center.tool_button_group.set_checked(0)
+
+        tab_left = MLineTabWidget(alignment=Qt.AlignLeft)
+        tab_left.add_tab(MLabel('test 1'), u'标签一')
+        tab_left.add_tab(MLabel('test 2'), u'标签二')
+        tab_left.add_tab(MLabel('test 3'), u'标签三')
+        tab_left.tool_button_group.set_checked(0)
+
+        tab_right = MLineTabWidget(alignment=Qt.AlignRight)
+        tab_right.add_tab(MLabel('test 1'), u'标签一')
+        tab_right.add_tab(MLabel('test 2'), u'标签二')
+        tab_right.add_tab(MLabel('test 3'), u'标签三')
+        tab_right.tool_button_group.set_checked(0)
+
+        main_lay.addWidget(MDivider('Center'))
+        main_lay.addWidget(tab_center)
+        main_lay.addWidget(MDivider('Left'))
+        main_lay.addWidget(tab_left)
+        main_lay.addWidget(MDivider('Right'))
+        main_lay.addWidget(tab_right)
+        main_lay.addStretch()
+        self.setLayout(main_lay)
+
+    @Slot()
+    def slot_close_tab(self, index):
+        if index > 0:
+            text = self.tab_left.tabText(index)
+            self.tab_left.removeTab(index)
+            MMessage.info({'content': u'成功关闭一个标签: {}'.format(text),
+                           'closable': True
+                           }, parent=self)
+        else:
+            MMessage.warning({'content': u'请不要关闭第一个标签',
+                              'closable': True
+                              }, parent=self)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    test = MLineTabWidgetTest()
+
+    from dayu_widgets import dayu_theme
+
+    dayu_theme.apply(test)
+    test.show()
+    sys.exit(app.exec_())
