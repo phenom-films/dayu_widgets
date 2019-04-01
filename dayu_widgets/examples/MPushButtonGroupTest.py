cd966e4b09426d67d8ab086525e2bc715e570673
muyanru <muyanru@phenom-films.com>
2019-04-01 22:31:20 +0800

    addStretch

--- dayu_widgets/examples/MPushButtonGroupTest.py
+++ dayu_widgets/examples/MPushButtonGroupTest.py
@@ -23,22 +23,28 @@ class MPushButtonGroupTest(QWidget, MFieldMixin):
     def _init_ui(self):
         button_config_list = [
             {'text': 'Add', 'icon': MIcon('add_line.svg', '#fff'), 'type': MPushButton.PrimaryType},
-            {'text': 'Edit', 'icon': MIcon('edit_fill.svg', '#fff'), 'type': MPushButton.InfoType},
+            {'text': 'Edit', 'icon': MIcon('edit_fill.svg', '#fff'), 'type': MPushButton.WarningType},
             {'text': 'Delete', 'icon': MIcon('trash_line.svg', '#fff'), 'type': MPushButton.ErrorType},
         ]
-        button_group_h = MPushButtonGroup(size=dayu_theme.size.small)
+        button_group_h = MPushButtonGroup(size=dayu_theme.large)
         button_group_h.set_button_list(button_config_list)
+        h_lay = QHBoxLayout()
+        h_lay.addWidget(button_group_h)
+        h_lay.addStretch()
 
         button_group_v = MPushButtonGroup(orientation=Qt.Vertical)
         button_group_v.set_button_list(button_config_list)
+        h_lay_2 = QHBoxLayout()
+        h_lay_2.addWidget(button_group_v)
+        h_lay_2.addStretch()
 
         main_lay = QVBoxLayout()
         main_lay.addWidget(
             MLabel(u'MPushButtonGroup is MPushButton collection. they are not exclusive.'))
         main_lay.addWidget(MDivider('MPushButton group: Horizontal & Small Size'))
-        main_lay.addWidget(button_group_h)
+        main_lay.addLayout(h_lay)
         main_lay.addWidget(MDivider('MPushButton group: Vertical & Default Size'))
-        main_lay.addWidget(button_group_v)
+        main_lay.addLayout(h_lay_2)
         main_lay.addStretch()
         self.setLayout(main_lay)
 
