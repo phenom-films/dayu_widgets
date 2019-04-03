966b0585c96af200a7fc25f7eb7e2612fd202a1f
muyanru <muyanru@phenom-films.com>
2019-04-03 20:27:48 +0800

    add animation

--- dayu_widgets/MTabWidget.py
+++ dayu_widgets/MTabWidget.py
@@ -27,5 +27,42 @@ class MTabBar(QTabBar):
 class MTabWidget(QTabWidget):
     def __init__(self, parent=None):
         super(MTabWidget, self).__init__(parent=parent)
+        self._previous_index = 0
         self.bar = MTabBar()
         self.setTabBar(self.bar)
+        self.currentChanged.connect(self.play_anim)
+
+        self.to_show_pos_ani = QPropertyAnimation()
+        self.to_show_pos_ani.setDuration(400)
+        self.to_show_pos_ani.setPropertyName('pos')
+        self.to_show_pos_ani.setEndValue(QPoint(0, 0))
+        self.to_show_pos_ani.setEasingCurve(QEasingCurve.OutCubic)
+
+        self.to_hide_pos_ani = QPropertyAnimation()
+        self.to_hide_pos_ani.setDuration(400)
+        self.to_hide_pos_ani.setPropertyName('pos')
+        self.to_hide_pos_ani.setEndValue(QPoint(0, 0))
+        self.to_hide_pos_ani.setEasingCurve(QEasingCurve.OutCubic)
+
+        self.opacity_eff = QGraphicsOpacityEffect()
+        self.opacity_ani = QPropertyAnimation()
+        self.opacity_ani.setDuration(400)
+        self.opacity_ani.setEasingCurve(QEasingCurve.InCubic)
+        self.opacity_ani.setPropertyName('opacity')
+        self.opacity_ani.setStartValue(0.0)
+        self.opacity_ani.setEndValue(1.0)
+        self.opacity_ani.setTargetObject(self.opacity_eff)
+
+    def play_anim(self, index):
+        current_widget = self.widget(index)
+        if self._previous_index < index:
+            self.to_show_pos_ani.setStartValue(QPoint(self.width(), 0))
+            self.to_show_pos_ani.setTargetObject(current_widget)
+            self.to_show_pos_ani.start()
+        else:
+            self.to_hide_pos_ani.setStartValue(QPoint(-self.width(), 0))
+            self.to_hide_pos_ani.setTargetObject(current_widget)
+            self.to_hide_pos_ani.start()
+        current_widget.setGraphicsEffect(self.opacity_eff)
+        self.opacity_ani.start()
+        self._previous_index = index
