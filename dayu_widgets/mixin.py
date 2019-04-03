d3beeb89fdc94f7f8d7e172b00601855796534f9
muyanru <muyanru@phenom-films.com>
2019-04-03 16:11:00 +0800

    add focus_shadow_mixin

--- dayu_widgets/mixin.py
+++ dayu_widgets/mixin.py
@@ -23,14 +23,46 @@ def property_mixin(cls):
 
 
 def cursor_mixin(cls):
+    old_enter_event = cls.enterEvent
+    old_leave_event = cls.leaveEvent
     def enterEvent(self, *args, **kwargs):
+        old_enter_event(self, *args, **kwargs)
         QApplication.setOverrideCursor(Qt.PointingHandCursor if self.isEnabled() else Qt.ForbiddenCursor)
         return super(cls, self).enterEvent(*args, **kwargs)
 
     def leaveEvent(self, *args, **kwargs):
+        old_leave_event(self, *args, **kwargs)
         QApplication.restoreOverrideCursor()
         return super(cls, self).leaveEvent(*args, **kwargs)
 
     setattr(cls, 'enterEvent', enterEvent)
     setattr(cls, 'leaveEvent', leaveEvent)
+    return cls
+
+
+def focus_shadow_mixin(cls):
+    old_focus_in_event = cls.focusInEvent
+    old_focus_out_event = cls.focusOutEvent
+    def focusInEvent(self, *args, **kwargs):
+        old_focus_in_event(self, *args, **kwargs)
+        if not self.graphicsEffect():
+            from dayu_widgets import dayu_theme
+            shadow_effect = QGraphicsDropShadowEffect(self)
+            dayu_type = self.property('type')
+            color = vars(dayu_theme).get('{}_color'.format(dayu_type or 'primary'))
+            shadow_effect.setColor(color)
+            shadow_effect.setOffset(0, 0)
+            shadow_effect.setBlurRadius(5)
+            shadow_effect.setEnabled(False)
+            self.setGraphicsEffect(shadow_effect)
+        if self.isEnabled():
+            self.graphicsEffect().setEnabled(True)
+
+    def focusOutEvent(self, *args, **kwargs):
+        old_focus_out_event(self, *args, **kwargs)
+        if self.graphicsEffect():
+            self.graphicsEffect().setEnabled(False)
+
+    setattr(cls, 'focusInEvent', focusInEvent)
+    setattr(cls, 'focusOutEvent', focusOutEvent)
     return cls
