5f9b459d95b4402dbd9debdbb006c8a0d1d578da
muyanru <muyanru@phenom-films.com>
2019-04-09 19:33:11 +0800

    refactor stacked widget currentChanged animation

--- dayu_widgets/MTabWidget.py
+++ dayu_widgets/MTabWidget.py
@@ -6,7 +6,7 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from dayu_widgets.mixin import cursor_mixin
+from dayu_widgets.mixin import cursor_mixin, stacked_animation_mixin
 from dayu_widgets.qt import *
 
 
@@ -24,45 +24,9 @@ class MTabBar(QTabBar):
             return QSize(self.fontMetrics().width(tab_text) + 50, self.fontMetrics().height() + 20)
 
 
+@stacked_animation_mixin
 class MTabWidget(QTabWidget):
     def __init__(self, parent=None):
         super(MTabWidget, self).__init__(parent=parent)
-        self._previous_index = 0
         self.bar = MTabBar()
         self.setTabBar(self.bar)
-        self.currentChanged.connect(self.play_anim)
-
-        self.to_show_pos_ani = QPropertyAnimation()
-        self.to_show_pos_ani.setDuration(400)
-        self.to_show_pos_ani.setPropertyName('pos')
-        self.to_show_pos_ani.setEndValue(QPoint(0, 0))
-        self.to_show_pos_ani.setEasingCurve(QEasingCurve.OutCubic)
-
-        self.to_hide_pos_ani = QPropertyAnimation()
-        self.to_hide_pos_ani.setDuration(400)
-        self.to_hide_pos_ani.setPropertyName('pos')
-        self.to_hide_pos_ani.setEndValue(QPoint(0, 0))
-        self.to_hide_pos_ani.setEasingCurve(QEasingCurve.OutCubic)
-
-        self.opacity_eff = QGraphicsOpacityEffect()
-        self.opacity_ani = QPropertyAnimation()
-        self.opacity_ani.setDuration(400)
-        self.opacity_ani.setEasingCurve(QEasingCurve.InCubic)
-        self.opacity_ani.setPropertyName('opacity')
-        self.opacity_ani.setStartValue(0.0)
-        self.opacity_ani.setEndValue(1.0)
-        self.opacity_ani.setTargetObject(self.opacity_eff)
-
-    def play_anim(self, index):
-        current_widget = self.widget(index)
-        if self._previous_index < index:
-            self.to_show_pos_ani.setStartValue(QPoint(self.width(), 0))
-            self.to_show_pos_ani.setTargetObject(current_widget)
-            self.to_show_pos_ani.start()
-        else:
-            self.to_hide_pos_ani.setStartValue(QPoint(-self.width(), 0))
-            self.to_hide_pos_ani.setTargetObject(current_widget)
-            self.to_hide_pos_ani.start()
-        current_widget.setGraphicsEffect(self.opacity_eff)
-        self.opacity_ani.start()
-        self._previous_index = index
