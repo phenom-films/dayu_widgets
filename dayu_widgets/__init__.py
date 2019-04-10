907890d725d335998ceca9af5160365f337d7922
muyanru <muyanru@phenom-films.com>
2019-04-10 17:06:07 +0800

    import components

--- dayu_widgets/__init__.py
+++ dayu_widgets/__init__.py
@@ -3,8 +3,8 @@ import os
 STATIC_FOLDERS = [os.path.join(__path__[0], 'static')]
 from MTheme import MTheme
 
-# dayu_theme = MTheme('dark')
-dayu_theme = MTheme()
+# dayu_theme = MTheme('dark', primary_color=MTheme.orange)
+dayu_theme = MTheme('light', primary_color='#722ed1')
 
 from MAbstractSpinBox import MSpinBox, MDoubleSpinBox, MDateTimeEdit, MDateEdit, MTimeEdit
 from MAlert import MAlert
@@ -15,27 +15,34 @@ from MBrowser import MClickBrowserFilePushButton, MClickBrowserFileToolButton, \
     MClickBrowserFolderPushButton, MClickBrowserFolderToolButton, \
     MDragFileButton, MDragFolderButton
 from MButtonGroup import MPushButtonGroup, MRadioButtonGroup, MCheckBoxGroup, MToolButtonGroup
+from MCard import MCard, MMeta
+from MCarousel import MCarousel
 from MCheckBox import MCheckBox
 from MCircle import MCircle
 from MCollapse import MCollapse
 from MComboBox import MComboBox
 from MDivider import MDivider
 from MFieldMixin import MFieldMixin
+from MFlowLayout import MFlowLayout
+from MItemModel import MTableModel, MSortFilterModel
+from MItemView import MTableView, MTreeView, MBigView, MListView
+from MItemViewFullSet import MItemViewFullSet
+from MItemViewSet import MItemViewSet
 from MLabel import MLabel
 from MLineEdit import MLineEdit
+from MLineTabWidget import MLineTabWidget
+from MLoading import MLoading, MLoadingWrapper
 from MMenu import MMenu
+from MMenuTabWidget import MMenuTabWidget
 from MMessage import MMessage
+from MPage import MPage
 from MProgressBar import MProgressBar
 from MPushButton import MPushButton
 from MRadioButton import MRadioButton
 from MSequenceFile import MSequenceFile
 from MSlider import MSlider
 from MSwitch import MSwitch
-from MItemView import MTableView, MTreeView, MBigView, MListView
-from MItemModel import MTableModel, MSortFilterModel
-from MItemViewSet import MItemViewSet
-from MItemViewFullSet import MItemViewFullSet
 from MTabWidget import MTabWidget
-from MMenuTabWidget import MMenuTabWidget
+from MTag import MTag, MCheckableTag, MNewTag
+from MToast import MToast
 from MToolButton import MToolButton
-from MLineTabWidget import MLineTabWidget
