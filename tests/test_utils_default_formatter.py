7457d0870ed4fbf496121b1c8da688fabb8c1a1c
muyanru <muyanru@phenom-films.com>
2019-05-29 12:29:05 +0800

    add test when input is list for default_formatter test

--- tests/test_utils_default_formatter.py
+++ tests/test_utils_default_formatter.py
@@ -37,12 +37,19 @@ class MyNoNameAndCodeObject(object):
 
 @pytest.mark.parametrize('input_value, result', (
         ({'name': 'Jim', 'age': 18}, 'Jim'),
+        ([{'name': 'Jim', 'age': 18}, {'name': 'Tom', 'age': 16}], 'Jim,Tom'),
         ({'code': 'pl_0010', 'id': 111}, 'pl_0010'),
+        ([{'code': 'pl_0010', 'id': 111}, {'code': 'pl_0020', 'id': 112}], 'pl_0010,pl_0020'),
         ({'id': 111, 'age': 18}, "{'age': 18, 'id': 111}"),
+        ([{'id': 111, 'age': 18}, {'id': 112, 'age': 19}], "{'age': 18, 'id': 111},{'age': 19, 'id': 112}"),
         ('test', 'test'),
+        (['test', 'test2'], 'test,test2'),
         (u'test', u'test'),
+        ([u'test', 'test2'], 'test,test2'),
         (None, '--'),
+        ([None, 'hello'], '--,hello'),
         (MyHasNameObject('Jim', 18), 'Jim'),
+        ([MyHasNameObject('Jim', 18), MyHasNameObject('Tom', 19)], 'Jim,Tom'),
         (MyHasCodeObject('pl_0010', 18), 'pl_0010'),
         (MyHasNameAndCodeObject('Jim', 'pl_0010', 18), 'Jim'),
         (MyNoNameAndCodeObject(), 'MyNoNameAndCodeObject()'),
@@ -50,6 +57,7 @@ class MyNoNameAndCodeObject(object):
         (20, '20'),
         (20.051, '20.05'),
         (20.058, '20.06'),
+        ([20, 20.058], '20,20.06'),
 ))
 def test_utils_default_formatter(input_value, result):
     assert utils.default_formatter(input_value) == result
