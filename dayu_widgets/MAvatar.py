ea28950b073d9e6f7a1c6f0f2c80a39308b42073
muyanru <muyanru@phenom-films.com>
2019-06-03 19:58:24 +0800

    refactor NAvatar set_image

--- dayu_widgets/MAvatar.py
+++ dayu_widgets/MAvatar.py
@@ -13,24 +13,25 @@ from dayu_widgets.qt import *
 
 @property_mixin
 class MAvatar(QLabel):
-    '''
-    自定义 props:
-        image:
-    '''
+    """
+    Property:
+    image: avatar image, should be QPixmap.
+    """
 
     def __init__(self, size=None, image=None, parent=None, flags=0):
         super(MAvatar, self).__init__(parent, flags)
         self._default_pix = MPixmap('user_fill.svg')
         size = size or dayu_theme.default_size
         self.setFixedSize(QSize(size, size))
-        self.set_image(image or '')
+        self.set_image(image)
 
     def _set_image(self, value):
-        fixed_height = self.height()
-        if value and isinstance(value, QPixmap) and (not value.isNull()):
-            self.setPixmap(value.scaledToWidth(fixed_height, Qt.SmoothTransformation))
-        else:
-            self.setPixmap(self._default_pix.scaledToWidth(fixed_height, Qt.SmoothTransformation))
+        self.setPixmap(value.scaledToWidth(self.height(), Qt.SmoothTransformation))
 
     def set_image(self, value):
-        self.setProperty('image', value)
+        if value is None:
+            self.setProperty('image', self._default_pix)
+        elif isinstance(value, QPixmap) and (not value.isNull()):
+            self.setProperty('image', value)
+        else:
+            raise TypeError("Input argument 'value' should be QPixmap or None, but get {}".format(type(value)))
