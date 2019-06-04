bd9635e50d5ab23cd27c327ac63128fc2e00b4f4
muyanru <muyanru@phenom-films.com>
2019-06-04 20:04:25 +0800

    use new set_content

--- dayu_widgets/examples/MBadgeTest.py
+++ dayu_widgets/examples/MBadgeTest.py
@@ -25,17 +25,28 @@ class MBadgeTest(QWidget, MFieldMixin):
         self._init_ui()
 
     def _init_ui(self):
+        standalone_lay = QHBoxLayout()
+        standalone_lay.addWidget(MBadge(content=0))
+        standalone_lay.addWidget(MBadge(content=20))
+        standalone_lay.addWidget(MBadge(content=100))
+        standalone_lay.addWidget(MBadge(content=True))
+        standalone_lay.addWidget(MBadge(content='new'))
+        standalone_lay.addStretch()
+
         button = MToolButton(icon=MIcon('trash_line.svg'))
         avatar = MAvatar(size=dayu_theme.large, image=MPixmap('avatar.png'))
         button_alert = MToolButton(icon=MIcon('alert_fill.svg'), size=dayu_theme.large)
-        badge_1 = MBadge(widget=button, dot=True)
-        badge_2 = MBadge(widget=avatar, dot=True)
+        badge_1 = MBadge(widget=button)
+        badge_1.set_content(True)
+        badge_2 = MBadge(widget=avatar)
+        badge_2.set_content(True)
         badge_3 = MBadge(widget=button_alert)
-        button.clicked.connect(lambda: badge_1.set_dot(False))
+        # badge_2.set_content(True)
+        button.clicked.connect(lambda: badge_1.set_content(False))
 
         spin_box = MSpinBox()
         spin_box.setRange(0, 9999)
-        spin_box.valueChanged.connect(lambda x: badge_3.set_count(x))
+        spin_box.valueChanged.connect(badge_3.set_content)
         spin_box.setValue(1)
 
         self.register_field('button1_selected', u'北京')
@@ -45,7 +56,8 @@ class MBadgeTest(QWidget, MFieldMixin):
         select1.set_menu(menu1)
         self.bind('button1_selected', select1, 'value', signal='sig_value_changed')
 
-        badge_hot = MBadge(widget=MLabel(u'你的理想城市'), text='hot')
+        badge_hot = MBadge(widget=MLabel(u'你的理想城市  '))
+        badge_hot.set_content('hot')
 
         sub_lay1 = QHBoxLayout()
         sub_lay1.addWidget(badge_1)
@@ -59,6 +71,8 @@ class MBadgeTest(QWidget, MFieldMixin):
         sub_lay2.addStretch()
 
         main_lay = QVBoxLayout()
+        main_lay.addWidget(MDivider('use standalone'))
+        main_lay.addLayout(standalone_lay)
         main_lay.addWidget(MDivider('different type'))
         main_lay.addLayout(sub_lay1)
         main_lay.addWidget(spin_box)
