1b4b86976e1c80b1112aaebeea55a42fd63afb4e
muyanru <muyanru@phenom-films.com>
2019-06-14 15:35:29 +0800

    optimize test init, and use pylint

--- tests/test_malert.py
+++ tests/test_malert.py
@@ -1,29 +1,32 @@
-from dayu_widgets.MAlert import MAlert
+"""Test MAlert class"""
 import pytest
 
-type_list = (None,
+from dayu_widgets.MAlert import MAlert
+
+TYPE_LIST = (None,
              MAlert.InfoType,
              MAlert.SuccessType,
              MAlert.WarningType,
              MAlert.ErrorType)
 
-test_input = []
-test_result = []
+TEST_INPUT = []
+TEST_OUTPUT = []
 for text in (None, '', 'test'):
     input_dict = {} if text is None else {'text': text}
     result_dict = {'text': '' if text is None else text, 'visible': bool(text)}
-    for t in type_list:
+    for t in TYPE_LIST:
         if t is not None:
             input_dict.update({'type': t})
         result_dict.update({'type': t or 'info'})
-        test_input.append(input_dict)
-        test_result.append(result_dict)
+        TEST_INPUT.append(input_dict)
+        TEST_OUTPUT.append(result_dict)
 
 
 @pytest.mark.parametrize('kwargs,result', [
-    (i, r) for i, r in zip(test_input, test_result)
+    (i, r) for i, r in zip(TEST_INPUT, TEST_OUTPUT)
 ])
 def test_malert_init(qtbot, kwargs, result):
+    """Test MAlert with different arguments."""
     widget = MAlert(**kwargs)
     qtbot.addWidget(widget)
 
@@ -34,26 +37,30 @@ def test_malert_init(qtbot, kwargs, result):
 
 @pytest.mark.parametrize('input_type', ('infos', 3, None, {'name': 'test'}))
 def test_malert_with_wrong_type(qtbot, input_type):
+    """Test MAlert with wrong type for type arg"""
     with pytest.raises(ValueError) as exc_info:
         widget = MAlert(type=input_type)
         qtbot.addWidget(widget)
 
     exception_msg = exc_info.value.args[0]
-    assert exception_msg == "Input argument 'value' should be one of info/success/warning/error string."
+    assert exception_msg == "Input argument 'value' should be " \
+                            "one of info/success/warning/error string."
 
 
 @pytest.mark.parametrize('input_text, error_type', (
-        (3, 'int'),
-        ([], 'list'),
-        ((1,), 'tuple'),
-        (set(), 'set'),
-        ({}, 'dict'),
-        (object(), 'object'),
+    (3, 'int'),
+    ([], 'list'),
+    ((1,), 'tuple'),
+    (set(), 'set'),
+    ({}, 'dict'),
+    (object(), 'object'),
 ))
-def test_malert_with_wrong_text_type(qtbot, input_text, error_type):
+def test_malert_with_wrong_text(qtbot, input_text, error_type):
+    """Test MAlert with wrong type for text arg."""
     with pytest.raises(TypeError) as exc_info:
         widget = MAlert(text=input_text)
         qtbot.addWidget(widget)
 
     exception_msg = exc_info.value.args[0]
-    assert exception_msg == "Input argument 'value' should be string type, but get <type '{}'>".format(error_type)
+    assert exception_msg == "Input argument 'value' should be string type, " \
+                            "but get <type '{}'>".format(error_type)
