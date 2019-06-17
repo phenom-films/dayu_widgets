300f9095c5f33c4b8362043fdb511c975f274a88
muyanru <muyanru@phenom-films.com>
2019-06-17 17:19:14 +0800

    optimize test. apply pylint

--- tests/test_utils_overflow_format.py
+++ tests/test_utils_overflow_format.py
@@ -1,29 +1,50 @@
-from dayu_widgets import utils
+"""
+Test overflow_format.
+"""
 import pytest
+
+from dayu_widgets import utils
 
 
 @pytest.mark.parametrize('num, overflow, result', (
-        (0, 99, '0'),
-        (100, 99, '99+'),
-        (20, 99, '20'),
-        (20, 50, '20'),
-        (-1, 50, '-1'),
-        (99, 50, '50+'),
+    (0, 99, '0'),
+    (100, 99, '99+'),
+    (20, 99, '20'),
+    (20, 50, '20'),
+    (-1, 50, '-1'),
+    (99, 50, '50+'),
 ))
 def test_overflow_format(num, overflow, result):
+    """Test overflow_format with normal arg."""
     assert utils.overflow_format(num, overflow) == result
 
 
-@pytest.mark.parametrize('num, overflow, arg, error_type', (
-        (0.0, 99, 'num', 'float'),
-        (100, 99.0, 'overflow', 'float'),
-        (20.0, 99.0, 'num', 'float'),
-        ('20', 50, 'num', 'str'),
-        (None, 50, 'num', 'NoneType')
+@pytest.mark.parametrize('num, overflow, error_type', (
+    (0.0, 99, 'float'),
+    (20.0, 99.0, 'float'),
+    ('20', 50, 'str'),
+    (None, 50, 'NoneType')
 ))
-def test_overflow_format_with_wrong_type(num, overflow, arg, error_type):
+def test_with_wrong_num_type(num, overflow, error_type):
+    """Test overflow_format with wrong """
     with pytest.raises(ValueError) as exc_info:
         utils.overflow_format(num, overflow)
 
     exception_msg = exc_info.value.args[0]
-    assert exception_msg == "Input argument '{}' should be int type, but get <type '{}'>".format(arg, error_type)
+    assert exception_msg == "Input argument 'num' should be int type, " \
+                            "but get <type '{}'>".format(error_type)
+
+
+@pytest.mark.parametrize('num, overflow, error_type', (
+    (100, 99.0, 'float'),
+    (10, '99', 'str'),
+    (0, None, 'NoneType'),
+))
+def test_with_wrong_overflow_type(num, overflow, error_type):
+    """Test overflow_format with wrong """
+    with pytest.raises(ValueError) as exc_info:
+        utils.overflow_format(num, overflow)
+
+    exception_msg = exc_info.value.args[0]
+    assert exception_msg == "Input argument 'overflow' should be int type, " \
+                            "but get <type '{}'>".format(error_type)
