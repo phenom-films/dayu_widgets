7602e1450b130ba97ab0da416b52be0aa1778aa0
muyanru <muyanru@phenom-films.com>
2019-06-18 12:02:21 +0800

    refactor avatar

--- tests/test_avatar.py
+++ tests/test_avatar.py
@@ -2,7 +2,7 @@
 Test MAvatar class
 """
 import pytest
-from dayu_widgets.MAvatar import MAvatar
+from dayu_widgets.avatar import MAvatar
 from dayu_widgets import dayu_theme
 from dayu_widgets.qt import MPixmap
 
@@ -10,7 +10,6 @@ from dayu_widgets.qt import MPixmap
 @pytest.mark.parametrize('image', ('check.svg', None))
 @pytest.mark.parametrize('size,result', (
     (None, dayu_theme.default_size),
-    (dayu_theme.default_size, dayu_theme.default_size),
     (dayu_theme.tiny, dayu_theme.tiny),
     (dayu_theme.small, dayu_theme.small),
     (dayu_theme.medium, dayu_theme.medium),
@@ -19,10 +18,11 @@ from dayu_widgets.qt import MPixmap
 ))
 def test_mavatar_init(qtbot, size, result, image):
     """Test for MAvatar init with different args"""
+    widget = MAvatar()
     if image:
-        widget = MAvatar(size=size, image=MPixmap(image))
-    else:
-        widget = MAvatar(size=size)
+        widget.set_dayu_image(MPixmap(image))
+    if size:
+        widget.set_dayu_size(size)
 
     qtbot.addWidget(widget)
 
@@ -35,7 +35,7 @@ def test_mavatar_init(qtbot, size, result, image):
     assert pix.width() == result
 
     orig = MPixmap('sphere.svg')
-    widget.set_image(orig)
+    widget.set_dayu_image(orig)
     pix = widget.pixmap()
     assert pix is not None
     assert not pix.isNull()
@@ -55,7 +55,8 @@ def test_mavatar_init(qtbot, size, result, image):
 def test_mavatar_with_wrong_image(qtbot, input_file, error_type):
     """Make sure when user give a wrong type arg, raise TypeError"""
     with pytest.raises(TypeError) as exc_info:
-        widget = MAvatar(image=input_file)
+        widget = MAvatar()
+        widget.set_dayu_image(input_file)
         qtbot.addWidget(widget)
 
     exception_msg = exc_info.value.args[0]
