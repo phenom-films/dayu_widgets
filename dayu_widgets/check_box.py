467b8ccb594511c1187e88a713e975e102770a4f
muyanru <muyanru@phenom-films.com>
2019-06-18 15:39:01 +0800

    refactor MCheckBox

--- dayu_widgets/check_box.py
+++ dayu_widgets/check_box.py
@@ -5,15 +5,17 @@
 # Date  : 2019.2
 # Email : muyanru345@163.com
 ###################################################################
+"""
+MCheckBox
+"""
+from dayu_widgets.mixin import cursor_mixin
+from dayu_widgets.qt import QCheckBox
 
-from dayu_widgets.mixin import property_mixin, cursor_mixin
-from dayu_widgets.qt import *
 
-
-@property_mixin
 @cursor_mixin
 class MCheckBox(QCheckBox):
-    def __init__(self, text='', icon=None, parent=None):
+    """
+    MCheckBox just use stylesheet and set cursor shape when hover. No more extend.
+    """
+    def __init__(self, text='', parent=None):
         super(MCheckBox, self).__init__(text=text, parent=parent)
-        if icon:
-            self.setIcon(icon)
