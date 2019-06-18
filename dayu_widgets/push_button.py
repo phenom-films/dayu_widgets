a28ef0ca198fb6e728aeb5845c0eb3ac8b3f1939
muyanru <muyanru@phenom-films.com>
2019-06-18 14:50:40 +0800

    refactor MPushButton

---
+++ dayu_widgets/push_button.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+"""
+MPushButton.
+"""
+from dayu_widgets import dayu_theme
+from dayu_widgets.mixin import cursor_mixin, focus_shadow_mixin
+from dayu_widgets.qt import QPushButton, Property
+
+
+@cursor_mixin
+@focus_shadow_mixin
+class MPushButton(QPushButton):
+    """
+    QPushButton.
+
+    Property:
+        dayu_size: The size of push button
+        dayu_type: The type of push button.
+    """
+    DefaultType = 'default'
+    PrimaryType = 'primary'
+    SuccessType = 'success'
+    WarningType = 'warning'
+    DangerType = 'danger'
+
+    def __init__(self, icon=None, text='', parent=None):
+        if icon:
+            super(MPushButton, self).__init__(icon=icon, text=text, parent=parent)
+        else:
+            super(MPushButton, self).__init__(text=text, parent=parent)
+        self._dayu_type = None
+        self._dayu_size = None
+        self.set_dayu_type(MPushButton.DefaultType)
+        self.set_dayu_size(dayu_theme.default_size)
+
+    def get_dayu_size(self):
+        """
+        Get the push button height
+        :return: integer
+        """
+        return self._dayu_size
+
+    def set_dayu_size(self, value):
+        """
+        Set the avatar size.
+        :param value: integer
+        :return: None
+        """
+        self._dayu_size = value
+        self.style().polish(self)
+
+    def get_dayu_type(self):
+        """
+        Get the push button type.
+        :return: string.
+        """
+        return self._dayu_type
+
+    def set_dayu_type(self, value):
+        """
+        Set the push button type.
+        :return: None
+        """
+        if value in [MPushButton.DefaultType,
+                     MPushButton.PrimaryType,
+                     MPushButton.SuccessType,
+                     MPushButton.WarningType,
+                     MPushButton.DangerType]:
+            self._dayu_type = value
+        else:
+            raise ValueError("Input argument 'value' should be one of "
+                             "default/primary/success/warning/danger string.")
+        self.style().polish(self)
+
+    @classmethod
+    def primary(cls, text='', icon=None, parent=None):
+        """Create a PrimaryType push button"""
+        ins = cls(text=text, icon=icon, parent=parent)
+        ins.set_dayu_type(cls.PrimaryType)
+        return ins
+
+    @classmethod
+    def success(cls, text='', icon=None, parent=None):
+        """Create a SuccessType push button"""
+        ins = cls(text=text, icon=icon, parent=parent)
+        ins.set_dayu_type(cls.SuccessType)
+        return ins
+
+    @classmethod
+    def warning(cls, text='', icon=None, parent=None):
+        """Create a WarningType push button"""
+        ins = cls(text=text, icon=icon, parent=parent)
+        ins.set_dayu_type(cls.WarningType)
+        return ins
+
+    @classmethod
+    def danger(cls, text='', icon=None, parent=None):
+        """Create a DangerType push button"""
+        ins = cls(text=text, icon=icon, parent=parent)
+        ins.set_dayu_type(cls.DangerType)
+        return ins
+
+    dayu_type = Property(str, get_dayu_type, set_dayu_type)
+    dayu_size = Property(int, get_dayu_size, set_dayu_size)
---
+++ examples/push_button_example.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.MDivider import MDivider
+from dayu_widgets.MFieldMixin import MFieldMixin
+from dayu_widgets.push_button import MPushButton
+from dayu_widgets.qt import QHBoxLayout, QVBoxLayout, QWidget, MIcon
+from dayu_widgets import dayu_theme
+
+
+class PushButtonExample(QWidget, MFieldMixin):
+    def __init__(self, parent=None):
+        super(PushButtonExample, self).__init__(parent)
+        self.setWindowTitle('Example for MPushButton')
+
+        sub_lay1 = QHBoxLayout()
+        sub_lay1.addWidget(MPushButton(text='Default'))
+        sub_lay1.addWidget(MPushButton.primary(text='Primary'))
+        sub_lay1.addWidget(MPushButton.success(text='Success'))
+        sub_lay1.addWidget(MPushButton.warning(text='Warning'))
+        sub_lay1.addWidget(MPushButton.danger(text='Danger'))
+
+        sub_lay2 = QHBoxLayout()
+        sub_lay2.addWidget(MPushButton(text='Upload', icon=MIcon('cloud_line.svg')))
+        sub_lay2.addWidget(
+            MPushButton.primary(text='Submit', icon=MIcon('folder_line.svg', '#ddd')))
+        sub_lay2.addWidget(
+            MPushButton.success(text='Submit', icon=MIcon('success_line.svg', '#ddd')))
+        sub_lay2.addWidget(MPushButton.warning(text='Edit', icon=MIcon('edit_line.svg', '#ddd')))
+        sub_lay2.addWidget(MPushButton.danger(text='Delete', icon=MIcon('trash_line.svg', '#ddd')))
+
+        sub_lay3 = QHBoxLayout()
+        size_list = [('Large', dayu_theme.large),
+                     ('Medium', dayu_theme.medium),
+                     ('Small', dayu_theme.small)]
+        for label, size in size_list:
+            button = MPushButton(text=label)
+            button.set_dayu_size(size)
+            button.set_dayu_type(MPushButton.PrimaryType)
+            sub_lay3.addWidget(button)
+
+        disabled_button = MPushButton(text='Disabled')
+        disabled_button.setEnabled(False)
+
+        main_lay = QVBoxLayout()
+        main_lay.addWidget(MDivider('different type'))
+        main_lay.addLayout(sub_lay1)
+        main_lay.addLayout(sub_lay2)
+        main_lay.addWidget(MDivider('different size'))
+        main_lay.addLayout(sub_lay3)
+        main_lay.addWidget(MDivider('disabled'))
+        main_lay.addWidget(disabled_button)
+        main_lay.addStretch()
+        self.setLayout(main_lay)
+
+
+if __name__ == '__main__':
+    import sys
+    from dayu_widgets.qt import QApplication
+
+    app = QApplication(sys.argv)
+    test = PushButtonExample()
+    # dayu_theme.set_theme('light')
+    dayu_theme.apply(test)
+    test.show()
+    sys.exit(app.exec_())
