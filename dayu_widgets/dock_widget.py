13469455d37149381ae641241ba4e9030489e54d
muyanru <muyanru@phenom-films.com>
2019-06-21 14:34:06 +0800

    add doc and refactor name

--- dayu_widgets/dock_widget.py
+++ dayu_widgets/dock_widget.py
@@ -5,9 +5,14 @@
 # Date  : 2019.3
 # Email : muyanru345@163.com
 ###################################################################
+"""MDockWidget"""
+from dayu_widgets.qt import QDockWidget
 
-from dayu_widgets.qt import *
 
 class MDockWidget(QDockWidget):
+    """
+    Just apply the qss. No more extend.
+    """
+
     def __init__(self, title='', parent=None, flags=0):
         super(MDockWidget, self).__init__(title, parent=parent, flags=flags)
