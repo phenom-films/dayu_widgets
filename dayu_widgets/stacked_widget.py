fec52c0fcc2520b6905960481b8f9fb4d03c895a
muyanru <muyanru@phenom-films.com>
2019-06-21 18:50:00 +0800

    add doc

--- dayu_widgets/stacked_widget.py
+++ dayu_widgets/stacked_widget.py
@@ -5,13 +5,15 @@
 # Date  : 2019.4
 # Email : muyanru345@163.com
 ###################################################################
-
+"""MStackedWidget"""
 
 from dayu_widgets.mixin import stacked_animation_mixin
-from dayu_widgets.qt import *
+from dayu_widgets.qt import QStackedWidget
 
 
 @stacked_animation_mixin
 class MStackedWidget(QStackedWidget):
+    """Just active animation when current index changed."""
+
     def __init__(self, parent=None):
         super(MStackedWidget, self).__init__(parent)
