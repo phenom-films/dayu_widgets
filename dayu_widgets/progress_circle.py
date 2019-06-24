31b78b06d37f77b901985d4b39eee513f1d421cd
muyanru <muyanru@phenom-films.com>
2019-06-24 17:49:19 +0800

    refactor MCircle

---
+++ dayu_widgets/progress_circle.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+"""MProgressCircle"""
+from dayu_widgets import dayu_theme
+from dayu_widgets import utils
+from dayu_widgets.label import MLabel
+from dayu_widgets.qt import QHBoxLayout, Qt, QSize, QPen, QPainter, Property, QProgressBar
+
+
+class MProgressCircle(QProgressBar):
+    """
+    MProgressCircle: Display the current progress of an operation flow.
+    When you need to display the completion percentage of an operation.
+
+    Property:
+        dayu_width: int
+        dayu_color: str
+    """
+
+    def __init__(self, dashboard=False, parent=None):
+        super(MProgressCircle, self).__init__(parent)
+        self._main_lay = QHBoxLayout()
+        self._default_label = MLabel.h3()
+        self._default_label.setAlignment(Qt.AlignCenter)
+        self._main_lay.addWidget(self._default_label)
+        self.setLayout(self._main_lay)
+        self._color = None
+        self._width = None
+
+        self._start_angle = 90 * 16
+        self._max_delta_angle = 360 * 16
+        self._height_factor = 1.0
+        self._width_factor = 1.0
+        if dashboard:
+            self._start_angle = 225 * 16
+            self._max_delta_angle = 270 * 16
+            self._height_factor = (2 + pow(2, 0.5)) / 4 + 0.03
+
+        self.set_dayu_width(120)
+        self.set_dayu_color(dayu_theme.primary_color)
+
+    def set_widget(self, widget):
+        """
+        Set a custom widget to show on the circle's inner center
+         and replace the default percent label
+        :param widget: QWidget
+        :return: None
+        """
+        self._default_label.setVisible(False)
+        self._main_lay.addWidget(widget)
+
+    def get_dayu_width(self):
+        """
+        Get current circle fixed width
+        :return: int
+        """
+        return self._width
+
+    def set_dayu_width(self, value):
+        """
+        Set current circle fixed width
+        :param value: int
+        :return: None
+        """
+        self._width = value
+        self.setFixedSize(QSize(self._width * self._width_factor,
+                                self._width * self._height_factor))
+
+    def get_dayu_color(self):
+        """
+        Get current circle foreground color
+        :return: str
+        """
+        return self._color
+
+    def set_dayu_color(self, value):
+        """
+        Set current circle's foreground color
+        :param value: str
+        :return:
+        """
+        self._color = value
+        self.update()
+
+    dayu_color = Property(str, get_dayu_color, set_dayu_color)
+    dayu_width = Property(int, get_dayu_width, set_dayu_width)
+
+    def paintEvent(self, event):
+        """Override QProgressBar's paintEvent."""
+        self._default_label.setText(self.text())
+        self._default_label.setVisible(self.isTextVisible())
+
+        percent = utils.get_percent(self.value(), self.minimum(), self.maximum())
+        total_width = self.get_dayu_width()
+        pen_width = int(3 * total_width / 50.0)
+        radius = total_width - pen_width - 1
+
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.Antialiasing)
+
+        # draw background circle
+        pen_background = QPen()
+        pen_background.setWidth(pen_width)
+        pen_background.setColor(dayu_theme.background_selected_color)
+        pen_background.setCapStyle(Qt.RoundCap)
+        painter.setPen(pen_background)
+        painter.drawArc(pen_width / 2.0 + 1,
+                        pen_width / 2.0 + 1,
+                        radius,
+                        radius,
+                        self._start_angle,
+                        -self._max_delta_angle)
+
+        # draw foreground circle
+        pen_foreground = QPen()
+        pen_foreground.setWidth(pen_width)
+        pen_foreground.setColor(self._color)
+        pen_foreground.setCapStyle(Qt.RoundCap)
+        painter.setPen(pen_foreground)
+        painter.drawArc(pen_width / 2.0 + 1,
+                        pen_width / 2.0 + 1,
+                        radius,
+                        radius,
+                        self._start_angle,
+                        -percent * 0.01 * self._max_delta_angle)
+        painter.end()
+
+    @classmethod
+    def dashboard(cls, parent=None):
+        """Create a dashboard style MCircle"""
+        return MProgressCircle(dashboard=True, parent=parent)
---
+++ examples/progress_circle_example.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.2
+# Email : muyanru345@163.com
+###################################################################
+
+import functools
+
+from dayu_widgets import dayu_theme
+from dayu_widgets.MButtonGroup import MPushButtonGroup
+from dayu_widgets.divider import MDivider
+from dayu_widgets.field_mixin import MFieldMixin
+from dayu_widgets.label import MLabel
+from dayu_widgets.progress_circle import MProgressCircle
+from dayu_widgets.push_button import MPushButton
+from dayu_widgets.qt import QWidget, QVBoxLayout, QHBoxLayout, Qt
+
+
+class ProgressCircleExample(QWidget, MFieldMixin):
+    def __init__(self, parent=None):
+        super(ProgressCircleExample, self).__init__(parent)
+        self.setWindowTitle('Examples for MProgressCircle')
+        self._init_ui()
+
+    def _init_ui(self):
+        main_lay = QVBoxLayout()
+        self.setLayout(main_lay)
+        main_lay.addWidget(MDivider('circle'))
+        lay1 = QHBoxLayout()
+        circle_1 = MProgressCircle(parent=self)
+        circle_1.setFormat(u'%p Days')
+        circle_1.setValue(80)
+        circle_2 = MProgressCircle(parent=self)
+        circle_2.set_dayu_color(dayu_theme.success_color)
+        circle_2.setValue(100)
+        circle_3 = MProgressCircle(parent=self)
+        circle_3.set_dayu_color(dayu_theme.error_color)
+        circle_3.setValue(40)
+
+        dashboard_1 = MProgressCircle.dashboard(parent=self)
+        dashboard_1.setFormat(u'%p Days')
+        dashboard_1.setValue(80)
+        dashboard_2 = MProgressCircle.dashboard(parent=self)
+        dashboard_2.set_dayu_color(dayu_theme.success_color)
+        dashboard_2.setValue(100)
+        dashboard_3 = MProgressCircle.dashboard(parent=self)
+        dashboard_3.set_dayu_color(dayu_theme.error_color)
+        dashboard_3.setValue(40)
+
+        lay1.addWidget(circle_1)
+        lay1.addWidget(circle_2)
+        lay1.addWidget(circle_3)
+
+        dashboard_lay = QHBoxLayout()
+        dashboard_lay.addWidget(dashboard_1)
+        dashboard_lay.addWidget(dashboard_2)
+        dashboard_lay.addWidget(dashboard_3)
+        main_lay.addLayout(lay1)
+        main_lay.addWidget(MDivider('dashboard'))
+        main_lay.addLayout(dashboard_lay)
+        main_lay.addWidget(MDivider('different radius'))
+
+        circle_4 = MProgressCircle(parent=self)
+        circle_4.set_dayu_width(100)
+        circle_4.setValue(40)
+        circle_5 = MProgressCircle(parent=self)
+        circle_5.setValue(40)
+        circle_6 = MProgressCircle(parent=self)
+        circle_6.set_dayu_width(160)
+        circle_6.setValue(40)
+        lay2 = QHBoxLayout()
+        lay2.addWidget(circle_4)
+        lay2.addWidget(circle_5)
+        lay2.addWidget(circle_6)
+
+        main_lay.addLayout(lay2)
+        main_lay.addWidget(MDivider('data bind'))
+
+        self.register_field('percent', 0)
+        self.register_field('color', self.get_color)
+        self.register_field('format', self.get_format)
+        circle = MProgressCircle(parent=self)
+
+        self.bind('percent', circle, 'value')
+        self.bind('color', circle, 'dayu_color')
+        self.bind('format', circle, 'format')
+        lay3 = QHBoxLayout()
+        button_grp = MPushButtonGroup()
+        button_grp.set_dayu_type(MPushButton.DefaultType)
+        button_grp.set_button_list([
+            {'text': '+', 'clicked': functools.partial(self.slot_change_percent, 10)},
+            {'text': '-', 'clicked': functools.partial(self.slot_change_percent, -10)},
+        ])
+        lay3.addWidget(circle)
+        lay3.addWidget(button_grp)
+        lay3.addStretch()
+        main_lay.addLayout(lay3)
+
+        custom_widget = QWidget()
+        custom_layout = QVBoxLayout()
+        custom_layout.setContentsMargins(20, 20, 20, 20)
+        custom_layout.addStretch()
+        custom_widget.setLayout(custom_layout)
+        lab1 = MLabel.h3(text='42,001,776')
+        lab2 = MLabel.secondary(text=u'消费人群规模')
+        lab3 = MLabel.secondary(text=u'总占人数 75%')
+        lab1.setAlignment(Qt.AlignCenter)
+        lab2.setAlignment(Qt.AlignCenter)
+        lab3.setAlignment(Qt.AlignCenter)
+        custom_layout.addWidget(lab1)
+        custom_layout.addWidget(lab2)
+        custom_layout.addWidget(MDivider())
+        custom_layout.addWidget(lab3)
+        custom_layout.addStretch()
+        custom_circle = MProgressCircle()
+        custom_circle.set_dayu_width(180)
+        custom_circle.setValue(75)
+        custom_circle.set_widget(custom_widget)
+
+        main_lay.addWidget(MDivider('custom circle'))
+        main_lay.addWidget(custom_circle)
+        main_lay.addStretch()
+
+    def get_color(self):
+        p = self.field('percent')
+        if p < 30:
+            return dayu_theme.error_color
+        if p < 60:
+            return dayu_theme.warning_color
+        if p < 100:
+            return dayu_theme.primary_color
+        return dayu_theme.success_color
+
+    def get_format(self):
+        p = self.field('percent')
+        if p < 30:
+            return u'>_<'
+        if p < 60:
+            return u'0_0'
+        if p < 100:
+            return u'^_^'
+        return u'^o^'
+
+    def slot_change_percent(self, value):
+        self.set_field('percent', max(0, min(self.field('percent') + value, 100)))
+
+
+if __name__ == '__main__':
+    import sys
+    from dayu_widgets.qt import QApplication
+
+    app = QApplication(sys.argv)
+    test = ProgressCircleExample()
+
+    dayu_theme.apply(test)
+    test.show()
+    sys.exit(app.exec_())
