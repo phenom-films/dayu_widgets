cc113d76015f948fe36bb6897c048b5e6d96e377
muyanru <muyanru@phenom-films.com>
2019-06-25 20:27:18 +0800

    add doc and some chain method

--- dayu_widgets/tool_button.py
+++ dayu_widgets/tool_button.py
@@ -5,45 +5,121 @@
 # Date  : 2019.2
 # Email : muyanru345@163.com
 ###################################################################
+"""MToolButton"""
 
 from dayu_widgets import dayu_theme
-from dayu_widgets.mixin import property_mixin, cursor_mixin
-from dayu_widgets.qt import QToolButton, QSizePolicy, QSize, Qt, Slot
+from dayu_widgets.mixin import cursor_mixin
+from dayu_widgets.qt import QToolButton, QSizePolicy, Qt, Slot, Property, MIcon
 
 
-@property_mixin
 @cursor_mixin
 class MToolButton(QToolButton):
-    NormalType = 'normal'
-    TaoBaoType = 'taobao'
-    IconOnlyType = 'icon'
-    BarType = 'bar'
-    BreadcrumbType = 'breadcrumb'
-    LineType = 'line'
+    """MToolButton"""
 
-    def __init__(self, text='', icon=None, icon_checked=None, type=None, size=None, parent=None):
+    def __init__(self, parent=None):
         super(MToolButton, self).__init__(parent=parent)
-        self.setProperty('icon_unchecked', icon)
-        self.setProperty('icon_checked', icon_checked or icon)
+        self._dayu_svg = None
         self.setAutoExclusive(False)
         self.setAutoRaise(True)
+
         self._polish_icon()
         self.toggled.connect(self._polish_icon)
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        size = size or dayu_theme.default_size
-        self.setProperty('type', type or MToolButton.NormalType)
-        self.setProperty('dayu_size', size)
-        if type == MToolButton.IconOnlyType:
-            self.setFixedSize(QSize(size, size))
-            self.setToolButtonStyle(Qt.ToolButtonIconOnly)
-        else:
-            self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
-            self.setText(text)
-        if type == MToolButton.TaoBaoType:
-            self.setCheckable(True)
+
+        self._dayu_size = dayu_theme.default_size
 
     @Slot(bool)
     def _polish_icon(self, checked=None):
-        icon = self.property('icon_checked') if self.isChecked() else self.property('icon_unchecked')
-        if icon:
-            self.setIcon(icon)
+        if self._dayu_svg:
+            if self.isCheckable() and self.isChecked():
+                self.setIcon(MIcon(self._dayu_svg, dayu_theme.primary_color))
+            else:
+                self.setIcon(MIcon(self._dayu_svg))
+
+    def enterEvent(self, event):
+        """Override enter event to highlight the icon"""
+        if self._dayu_svg:
+            self.setIcon(MIcon(self._dayu_svg, dayu_theme.primary_color))
+        return super(MToolButton, self).enterEvent(event)
+
+    def leaveEvent(self, event):
+        """Override leave event to recover the icon"""
+        self._polish_icon()
+        return super(MToolButton, self).leaveEvent(event)
+
+    def get_dayu_size(self):
+        """
+        Get the push button height
+        :return: integer
+        """
+        return self._dayu_size
+
+    def set_dayu_size(self, value):
+        """
+        Set the avatar size.
+        :param value: integer
+        :return: None
+        """
+        self._dayu_size = value
+        self.style().polish(self)
+
+    def get_dayu_svg(self):
+        """Get current svg path"""
+        return self._dayu_svg
+
+    def set_dayu_svg(self, path):
+        """Set current svg path"""
+        self._dayu_svg = path
+        self._polish_icon()
+
+    dayu_size = Property(int, get_dayu_size, set_dayu_size)
+
+    def huge(self):
+        """Set MPushButton to PrimaryType"""
+        self.set_dayu_size(dayu_theme.huge)
+        return self
+
+    def large(self):
+        """Set MPushButton to SuccessType"""
+        self.set_dayu_size(dayu_theme.large)
+        return self
+
+    def medium(self):
+        """Set MPushButton to  WarningType"""
+        self.set_dayu_size(dayu_theme.medium)
+        return self
+
+    def small(self):
+        """Set MPushButton to DangerType"""
+        self.set_dayu_size(dayu_theme.small)
+        return self
+
+    def tiny(self):
+        """Set MPushButton to DangerType"""
+        self.set_dayu_size(dayu_theme.tiny)
+        return self
+
+    def svg(self, path):
+        """Set current svg path"""
+        self.set_dayu_svg(path)
+        return self
+
+    def icon_only(self):
+        """Set tool button style to icon only"""
+        self.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        return self
+
+    def text_only(self):
+        """Set tool button style to text only"""
+        self.setToolButtonStyle(Qt.ToolButtonTextOnly)
+        return self
+
+    def text_beside_icon(self):
+        """Set tool button style to text beside icon"""
+        self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        return self
+
+    def text_under_icon(self):
+        """Set tool button style to text under icon"""
+        self.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        return self
