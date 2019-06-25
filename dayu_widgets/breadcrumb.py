8b6e57effe6b22a0fb3ec5afd73a3df0ea5775f1
muyanru <muyanru@phenom-films.com>
2019-06-25 19:15:16 +0800

    add doc

--- dayu_widgets/breadcrumb.py
+++ dayu_widgets/breadcrumb.py
@@ -5,27 +5,37 @@
 # Date  : 2019.3
 # Email : muyanru345@163.com
 ###################################################################
+"""
+MBreadcrumb
+"""
 
 from dayu_widgets.label import MLabel
 from dayu_widgets.tool_button import MToolButton
-from dayu_widgets.qt import *
-from dayu_widgets import dayu_theme
+from dayu_widgets.qt import QHBoxLayout, QWidget, QSizePolicy, QButtonGroup
 
 
 class MBreadcrumb(QWidget):
-    def __init__(self, separator='/', size=None, parent=None):
+    """
+    MBreadcrumb
+
+    A breadcrumb displays the current location within a hierarchy.
+    It allows going back to states higher up in the hierarchy.
+    """
+
+    def __init__(self, separator='/', parent=None):
         super(MBreadcrumb, self).__init__(parent)
         self._separator = separator
         self._main_layout = QHBoxLayout()
         self._main_layout.setContentsMargins(0, 0, 0, 0)
+        self._main_layout.setSpacing(0)
         self._main_layout.addStretch()
         self.setLayout(self._main_layout)
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         self._button_group = QButtonGroup()
-        self._size = size or dayu_theme.default_size
         self._label_list = []
 
     def set_item_list(self, data_list):
+        """Set the whole breadcrumb items. It will clear the old widgets."""
         for button in self._button_group.buttons():
             self._button_group.removeButton(button)
             self._main_layout.removeWidget(button)
@@ -39,22 +49,25 @@ class MBreadcrumb(QWidget):
             self.add_item(data_dict, index)
 
     def add_item(self, data_dict, index=None):
-        if isinstance(data_dict, basestring):
-            data_dict = {'text': data_dict}
-        elif isinstance(data_dict, QIcon):
-            data_dict = {'icon': data_dict}
-        button = MToolButton(text=data_dict.get('text'),
-                             icon=data_dict.get('icon'),
-                             type=MToolButton.BreadcrumbType,
-                             size=data_dict.get('size') or self._size,
-                             parent=self)
+        """Add a item"""
+        button = MToolButton()
+        button.setText(data_dict.get('text'))
+        if data_dict.get('svg'):
+            button.svg(data_dict.get('svg'))
         if data_dict.get('tooltip'):
             button.setProperty('toolTip', data_dict.get('tooltip'))
         if data_dict.get('clicked'):
             button.clicked.connect(data_dict.get('clicked'))
+        if data_dict.get('text'):
+            if data_dict.get('svg') or data_dict.get('icon'):
+                button.text_beside_icon()
+            else:
+                button.text_only()
+        else:
+            button.icon_only()
 
-        if len(self._button_group.buttons()) != 0:
-            separator = MLabel.secondary(self._separator)
+        if self._button_group.buttons():
+            separator = MLabel(self._separator).secondary()
             self._label_list.append(separator)
             self._main_layout.insertWidget(self._main_layout.count() - 1, separator)
         self._main_layout.insertWidget(self._main_layout.count() - 1, button)
