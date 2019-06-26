eba2bb6f84b9e966299056f5c1c981f1d91b023d
muyanru <muyanru@phenom-films.com>
2019-06-26 19:36:58 +0800

    refactor and add doc

--- dayu_widgets/line_edit.py
+++ dayu_widgets/line_edit.py
@@ -5,90 +5,153 @@
 # Date  : 2019.2
 # Email : muyanru345@163.com
 ###################################################################
-
+"""MLineEdit
+Get the user input is a text field
+"""
 import functools
 
 from dayu_widgets import dayu_theme
 from dayu_widgets.browser import MClickBrowserFileToolButton, MClickBrowserFolderToolButton
+from dayu_widgets.mixin import focus_shadow_mixin
 from dayu_widgets.push_button import MPushButton
+from dayu_widgets.qt import QLineEdit, Signal, QHBoxLayout, Slot, QTextEdit, QApplication, Qt, \
+    QTimer, Property
 from dayu_widgets.tool_button import MToolButton
-from dayu_widgets.mixin import property_mixin, focus_shadow_mixin
-from dayu_widgets.qt import *
 
 
-@property_mixin
 @focus_shadow_mixin
 class MLineEdit(QLineEdit):
-    sig_delay_text_changed = Signal(unicode)
+    """MLineEdit"""
+    sig_delay_text_changed = Signal(basestring)
 
-    def __init__(self, text='', size=None, parent=None):
+    def __init__(self, text='', parent=None):
         super(MLineEdit, self).__init__(text, parent)
         self._main_layout = QHBoxLayout()
         self._main_layout.setContentsMargins(0, 0, 0, 0)
         self._main_layout.addStretch()
-        self.setProperty('dayu_size', size or dayu_theme.default_size)
+
+        self._prefix_widget = None
+        self._suffix_widget = None
+
         self.setLayout(self._main_layout)
         self.setProperty('history', self.property('text'))
         self.setTextMargins(2, 0, 2, 0)
-        self.delay_timer = QTimer()
-        self.delay_timer.setInterval(500)
-        self.delay_timer.setSingleShot(True)
-        self.delay_timer.timeout.connect(self._slot_delay_text_changed)
 
-    def set_delay_duration(self, ms):
-        self.delay_timer.setInterval(ms)
+        self._delay_timer = QTimer()
+        self._delay_timer.setInterval(500)
+        self._delay_timer.setSingleShot(True)
+        self._delay_timer.timeout.connect(self._slot_delay_text_changed)
+
+        self._dayu_size = dayu_theme.default_size
+
+    def get_dayu_size(self):
+        """
+        Get the push button height
+        :return: integer
+        """
+        return self._dayu_size
+
+    def set_dayu_size(self, value):
+        """
+        Set the avatar size.
+        :param value: integer
+        :return: None
+        """
+        self._dayu_size = value
+        if hasattr(self._prefix_widget, 'set_dayu_size'):
+            self._prefix_widget.set_dayu_size(self._dayu_size)
+        if hasattr(self._suffix_widget, 'set_dayu_size'):
+            self._suffix_widget.set_dayu_size(self._dayu_size)
+        self.style().polish(self)
+
+    dayu_size = Property(int, get_dayu_size, set_dayu_size)
+
+    def set_delay_duration(self, millisecond):
+        """Set delay timer's timeout duration."""
+        self._delay_timer.setInterval(millisecond)
 
     @Slot()
     def _slot_delay_text_changed(self):
         self.sig_delay_text_changed.emit(self.text())
 
-    def add_prefix_widget(self, widget):
-        if isinstance(widget, MPushButton):
-            widget.setProperty('combine', 'horizontal')
+    def get_prefix_widget(self):
+        """Get the prefix widget for user to edit"""
+        return self._prefix_widget
+
+    def set_prefix_widget(self, widget):
+        """Set the line edit left start widget"""
+        if self._prefix_widget:
+            index = self._main_layout.indexOf(self._prefix_widget)
+            self._main_layout.takeAt(index)
+            self._prefix_widget.setVisible(False)
+        # if isinstance(widget, MPushButton):
+        widget.setProperty('combine', 'horizontal')
+        widget.setProperty('position', 'left')
+        if hasattr(widget, 'set_dayu_size'):
+            widget.set_dayu_size(self._dayu_size)
+
         margin = self.textMargins()
         margin.setLeft(margin.left() + widget.width())
         self.setTextMargins(margin)
+
         self._main_layout.insertWidget(0, widget)
+        self._prefix_widget = widget
         return widget
 
-    def add_suffix_widget(self, widget):
-        if isinstance(widget, MPushButton):
-            widget.setProperty('combine', 'horizontal')
+    def get_suffix_widget(self):
+        """Get the suffix widget for user to edit"""
+        return self._suffix_widget
+
+    def set_suffix_widget(self, widget):
+        """Set the line edit right end widget"""
+        if self._suffix_widget:
+            index = self._main_layout.indexOf(self._suffix_widget)
+            self._main_layout.takeAt(index)
+            self._suffix_widget.setVisible(False)
+        # if isinstance(widget, MPushButton):
+        widget.setProperty('combine', 'horizontal')
+        widget.setProperty('position', 'right')
+        if hasattr(widget, 'set_dayu_size'):
+            widget.set_dayu_size(self._dayu_size)
+
         margin = self.textMargins()
         margin.setRight(margin.right() + widget.width())
         self.setTextMargins(margin)
         self._main_layout.addWidget(widget)
+        self._suffix_widget = widget
         return widget
 
     def setText(self, text):
+        """Override setText save text to history"""
         self.setProperty('history', u'{}\n{}'.format(self.property('history'), text))
         return super(MLineEdit, self).setText(text)
 
     def clear(self):
+        """Override clear to clear history"""
         self.setProperty('history', '')
         return super(MLineEdit, self).clear()
 
     def keyPressEvent(self, event):
+        """Override keyPressEvent to start delay timer"""
         if event.key() not in [Qt.Key_Enter, Qt.Key_Tab]:
-            if self.delay_timer.isActive():
-                self.delay_timer.stop()
-            self.delay_timer.start()
+            if self._delay_timer.isActive():
+                self._delay_timer.stop()
+            self._delay_timer.start()
         super(MLineEdit, self).keyPressEvent(event)
 
-    @classmethod
-    def search(cls, size=None, parent=None):
-        line_edit = MLineEdit(size=size, parent=parent)
-        suffix_button = MToolButton(type=MToolButton.IconOnlyType,
-                                    icon=MIcon('close_line.svg'), size=size, parent=parent)
-        suffix_button.clicked.connect(line_edit.clear)
-        line_edit.add_suffix_widget(suffix_button)
-        line_edit.setPlaceholderText(line_edit.tr('Enter key word to search...'))
-        return line_edit
+    def search(self):
+        """Add a search icon button for MLineEdit."""
+        suffix_button = MToolButton().icon_only().svg('close_line.svg')
+        suffix_button.clicked.connect(self.clear)
+        self.set_suffix_widget(suffix_button)
+        self.setPlaceholderText(self.tr('Enter key word to search...'))
+        return self
 
-    @classmethod
-    def error(cls, size=None, parent=None):
+    def error(self):
+        """A a toolset to MLineEdit to store error info with red style"""
+
         @Slot()
-        def slot_show_detail(self):
+        def _slot_show_detail(self):
             dialog = QTextEdit(self)
             dialog.setReadOnly(True)
             geo = QApplication.desktop().screenGeometry()
@@ -98,46 +161,63 @@ class MLineEdit(QLineEdit):
             dialog.setWindowFlags(Qt.Dialog)
             dialog.show()
 
-        line_edit = MLineEdit(size=size, parent=parent)
-        line_edit.setProperty('type', 'error')
-        line_edit.setReadOnly(True)
-        suffix_button = MToolButton(type=MToolButton.IconOnlyType,
-                                    icon=MIcon('detail_line.svg', dayu_theme.error_color),
-                                    size=size)
-        suffix_button.clicked.connect(functools.partial(slot_show_detail, line_edit))
-        line_edit.add_suffix_widget(suffix_button)
-        line_edit.setPlaceholderText(line_edit.tr('Error information will be here...'))
-        return line_edit
+        self.setProperty('dayu_type', 'error')
+        self.setReadOnly(True)
+        _suffix_button = MToolButton().icon_only().svg('detail_line.svg')
+        _suffix_button.clicked.connect(functools.partial(_slot_show_detail, self))
+        self.set_suffix_widget(_suffix_button)
+        self.setPlaceholderText(self.tr('Error information will be here...'))
+        return self
 
-    @classmethod
-    def search_engine(cls, size=None, parent=None):
-        line_edit = MLineEdit(size=size, parent=parent)
-        suffix_button = MPushButton.primary(text='Search')
-        suffix_button.set_dayu_size(size)
+    def search_engine(self):
+        """Add a MPushButton to suffix for MLineEdit"""
+        _suffix_button = MPushButton(text='Search').primary()
+        _suffix_button.clicked.connect(self.returnPressed)
+        _suffix_button.setFixedWidth(100)
+        self.set_suffix_widget(_suffix_button)
+        self.setPlaceholderText(self.tr('Enter key word to search...'))
+        return self
 
-        suffix_button.clicked.connect(line_edit.returnPressed)
-        suffix_button.setFixedWidth(100)
-        line_edit.add_suffix_widget(suffix_button)
-        line_edit.setPlaceholderText(line_edit.tr('Enter key word to search...'))
-        return line_edit
+    def file(self, filters=None):
+        """Add a MClickBrowserFileToolButton for MLineEdit to select file"""
+        _suffix_button = MClickBrowserFileToolButton()
+        _suffix_button.sig_file_changed.connect(self.setText)
+        _suffix_button.set_dayu_filters(filters or [])
+        self.textChanged.connect(_suffix_button.set_dayu_path)
+        self.set_suffix_widget(_suffix_button)
+        self.setPlaceholderText(self.tr('Click button to browser files'))
+        return self
 
-    @classmethod
-    def file(cls, size=None, format=None, parent=None):
-        line_edit = MLineEdit(size=size, parent=parent)
-        suffix_button = MClickBrowserFileToolButton(size=size)
-        suffix_button.sig_file_changed.connect(line_edit.setText)
-        suffix_button.set_format(format or [])
-        line_edit.textChanged.connect(suffix_button.set_path)
-        line_edit.add_suffix_widget(suffix_button)
-        line_edit.setPlaceholderText(line_edit.tr('Click button to browser files'))
-        return line_edit
+    def folder(self):
+        """Add a MClickBrowserFolderToolButton for MLineEdit to select folder"""
+        _suffix_button = MClickBrowserFolderToolButton()
+        _suffix_button.sig_folder_changed.connect(self.setText)
+        self.textChanged.connect(_suffix_button.set_dayu_path)
+        self.set_suffix_widget(_suffix_button)
+        self.setPlaceholderText(self.tr('Click button to browser folder'))
+        return self
 
-    @classmethod
-    def folder(cls, size=None, parent=None):
-        line_edit = MLineEdit(size=size, parent=parent)
-        suffix_button = MClickBrowserFolderToolButton(size=size)
-        suffix_button.sig_folder_changed.connect(line_edit.setText)
-        line_edit.textChanged.connect(suffix_button.set_path)
-        line_edit.add_suffix_widget(suffix_button)
-        line_edit.setPlaceholderText(line_edit.tr('Click button to browser folder'))
-        return line_edit
+    def huge(self):
+        """Set MLineEdit to huge size"""
+        self.set_dayu_size(dayu_theme.huge)
+        return self
+
+    def large(self):
+        """Set MLineEdit to large size"""
+        self.set_dayu_size(dayu_theme.large)
+        return self
+
+    def medium(self):
+        """Set MLineEdit to  medium"""
+        self.set_dayu_size(dayu_theme.medium)
+        return self
+
+    def small(self):
+        """Set MLineEdit to small size"""
+        self.set_dayu_size(dayu_theme.small)
+        return self
+
+    def tiny(self):
+        """Set MLineEdit to tiny size"""
+        self.set_dayu_size(dayu_theme.tiny)
+        return self
--- examples/line_edit_example.py
+++ examples/line_edit_example.py
@@ -6,55 +6,51 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from dayu_widgets.divider import MDivider
-from dayu_widgets.line_edit import MLineEdit
-from dayu_widgets.push_button import MPushButton
-from dayu_widgets.tool_button import MToolButton
-from dayu_widgets.message import MMessage
 from dayu_widgets.combo_box import MComboBox
-from dayu_widgets.menu import MMenu
+from dayu_widgets.divider import MDivider
 from dayu_widgets.label import MLabel
-from dayu_widgets import dayu_theme
-from dayu_widgets.qt import *
+from dayu_widgets.line_edit import MLineEdit
+from dayu_widgets.menu import MMenu
+from dayu_widgets.message import MMessage
+from dayu_widgets.push_button import MPushButton
+from dayu_widgets.qt import QWidget, QHBoxLayout, Qt, QVBoxLayout, Slot
+from dayu_widgets.tool_button import MToolButton
 
 
 class LineEditExample(QWidget):
     def __init__(self, parent=None):
         super(LineEditExample, self).__init__(parent)
+        self.setWindowTitle('Examples for MLineEdit')
         self._init_ui()
 
     def _init_ui(self):
-        size_list = [('Large', dayu_theme.large),
-                     ('Medium', dayu_theme.medium),
-                     ('Small', dayu_theme.small)]
-        size_lay = QVBoxLayout()
-        for label, size in size_list:
-            line_edit_size = MLineEdit(size=size)
-            line_edit_size.setPlaceholderText(label)
-            size_lay.addWidget(line_edit_size)
+        size_lay = QHBoxLayout()
+        line_edit_l = MLineEdit().large()
+        line_edit_l.setPlaceholderText('large size')
+        line_edit_m = MLineEdit().medium()
+        line_edit_m.setPlaceholderText('default size')
+        line_edit_s = MLineEdit().small()
+        line_edit_s.setPlaceholderText('small size')
+        size_lay.addWidget(line_edit_l)
+        size_lay.addWidget(line_edit_m)
+        size_lay.addWidget(line_edit_s)
 
-        line_edit_icon = MLineEdit(text='Xiao Hua', size=dayu_theme.small)
-        tool_button = MToolButton(type=MToolButton.IconOnlyType, icon=MIcon('female.svg'), size=dayu_theme.small)
-        line_edit_icon.add_suffix_widget(tool_button)
+        line_edit_tool_button = MLineEdit(text='MToolButton')
+        line_edit_tool_button.set_prefix_widget(MToolButton().svg('user_line.svg').icon_only())
 
-        line_edit_button = MLineEdit(text='Beijing', size=dayu_theme.small)
-        push_button = MPushButton(text='Go').primary().small()
+        line_edit_label = MLineEdit(text='MLabel')
+        tool_button = MLabel(text='User').mark().secondary()
+        tool_button.setAlignment(Qt.AlignCenter)
+        tool_button.setFixedWidth(80)
+        line_edit_label.set_prefix_widget(tool_button)
+
+        line_edit_push_button = MLineEdit(text='MPushButton')
+        push_button = MPushButton(text='Go').primary()
         push_button.setFixedWidth(40)
-        line_edit_button.add_suffix_widget(push_button)
+        line_edit_push_button.set_suffix_widget(push_button)
 
-        line_edit_error = MLineEdit.error(size=dayu_theme.small)
-        line_edit_error.setText('waring: file d:/ddd/ccc.jpg not exists.')
-
-        line_edit_search = MLineEdit.search(size=dayu_theme.small)
-        line_edit_search_engine = MLineEdit.search_engine(size=dayu_theme.large)
-        line_edit_search_engine.add_prefix_widget(MToolButton(type=MToolButton.IconOnlyType,
-                                                              icon=MIcon('filter_line.svg', '#cccccc'),
-                                                              size=dayu_theme.large))
-        line_edit_search_engine.returnPressed.connect(self.slot_search)
-        # line_edit_search_engine.sig_delay_text_changed.connect(self.slot_search)
-
-        line_edit_file = MLineEdit.file()
-        line_edit_folder = MLineEdit.folder()
+        search_engine_line_edit = MLineEdit().search_engine().large()
+        search_engine_line_edit.returnPressed.connect(self.slot_search)
 
         line_edit_options = MLineEdit()
         combobox = MComboBox()
@@ -64,26 +60,27 @@ class LineEditExample(QWidget):
         combobox.set_menu(option_menu)
         combobox.set_value('http://')
         combobox.setFixedWidth(90)
-        line_edit_options.add_prefix_widget(combobox)
+        line_edit_options.set_prefix_widget(combobox)
 
         main_lay = QVBoxLayout()
         main_lay.addWidget(MDivider('different size'))
         main_lay.addLayout(size_lay)
-        main_lay.addWidget(MDivider('icon/button'))
-        main_lay.addWidget(line_edit_icon)
-        main_lay.addWidget(line_edit_button)
+        main_lay.addWidget(MDivider('custom prefix and suffix widget'))
+        main_lay.addWidget(line_edit_tool_button)
+        main_lay.addWidget(line_edit_label)
+        main_lay.addWidget(line_edit_push_button)
         main_lay.addWidget(MDivider('preset'))
 
-        main_lay.addWidget(MLabel('MLineEdit.error()'))
-        main_lay.addWidget(line_edit_error)
-        main_lay.addWidget(MLabel('MLineEdit.search()'))
-        main_lay.addWidget(line_edit_search)
-        main_lay.addWidget(MLabel('MLineEdit.search_engine()'))
-        main_lay.addWidget(line_edit_search_engine)
-        main_lay.addWidget(MLabel('MLineEdit.file()'))
-        main_lay.addWidget(line_edit_file)
-        main_lay.addWidget(MLabel('MLineEdit.folder()'))
-        main_lay.addWidget(line_edit_folder)
+        main_lay.addWidget(MLabel('error'))
+        main_lay.addWidget(MLineEdit(text='waring: file d:/ddd/ccc.jpg not exists.').error())
+        main_lay.addWidget(MLabel('search'))
+        main_lay.addWidget(MLineEdit().search().small())
+        main_lay.addWidget(MLabel('search_engine'))
+        main_lay.addWidget(search_engine_line_edit)
+        main_lay.addWidget(MLabel('file'))
+        main_lay.addWidget(MLineEdit().file().small())
+        main_lay.addWidget(MLabel('folder'))
+        main_lay.addWidget(MLineEdit().folder().small())
         main_lay.addWidget(MLabel('MLineEdit.options()'))
         main_lay.addWidget(line_edit_options)
         main_lay.addStretch()
@@ -96,10 +93,11 @@ class LineEditExample(QWidget):
 
 if __name__ == '__main__':
     import sys
-
+    from dayu_widgets.qt import QApplication
+    from dayu_widgets import dayu_theme
     app = QApplication(sys.argv)
     test = LineEditExample()
-    from dayu_widgets import dayu_theme
+
     dayu_theme.apply(test)
     test.show()
     sys.exit(app.exec_())
