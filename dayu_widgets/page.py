e3f50b92c05b8348781d6c2d3e76bcfaefb5ada2
muyanru <muyanru@phenom-films.com>
2019-07-03 18:59:59 +0800

    add docs for page

--- dayu_widgets/page.py
+++ dayu_widgets/page.py
@@ -5,9 +5,8 @@
 # Date  : 2019.3
 # Email : muyanru345@163.com
 ###################################################################
-
+"""MPage"""
 import functools
-import math
 
 from dayu_widgets import dayu_theme
 from dayu_widgets.spin_box import MSpinBox
@@ -16,15 +15,20 @@ from dayu_widgets.field_mixin import MFieldMixin
 from dayu_widgets.label import MLabel
 from dayu_widgets.menu import MMenu
 from dayu_widgets.tool_button import MToolButton
-from dayu_widgets.qt import *
+from dayu_widgets import utils
+from dayu_widgets.qt import QWidget, Signal, Qt, QHBoxLayout
 
 
 class MPage(QWidget, MFieldMixin):
-    sig_need_update = Signal()
+    """
+    MPage
+    A long list can be divided into several pages by MPage,
+    and only one page will be loaded at a time.
+    """
+    sig_page_changed = Signal(int, int)
 
     def __init__(self, parent=None):
         super(MPage, self).__init__(parent)
-        size = dayu_theme.small
         self.register_field('page_size_selected', 25)
         self.register_field('page_size_list',
                             [{'label': '25 - Fastest', 'value': 25},
@@ -34,45 +38,46 @@ class MPage(QWidget, MFieldMixin):
         self.register_field('total', 0)
         self.register_field('current_page', 0)
         self.register_field('total_page',
-                            lambda: math.ceil(1.0 * self.field('total') / self.field('page_size_selected')))
+                            lambda: utils.get_total_page(self.field('total'),
+                                                         self.field('page_size_selected')))
+        self.register_field('total_page_text',
+                            lambda: str(self.field('total_page')))
         self.register_field('display_text',
-                            lambda: '{start} - {end} of {total}'.format(
-                                start=((self.field('current_page')-1) * self.field('page_size_selected') + 1) if self.field(
-                                    'current_page') else 0,
-                                end=min(self.field('total'),
-                                        self.field('current_page') * self.field('page_size_selected')),
-                                total=self.field('total')))
+                            lambda: utils.get_page_display_string(self.field('current_page'),
+                                                                  self.field('page_size_selected'),
+                                                                  self.field('total')))
         self.register_field('can_pre',
                             lambda: self.field('current_page') > 1)
         self.register_field('can_next',
                             lambda: self.field('current_page') < self.field('total_page'))
-        # self.register_field('anything_changed', self.anything_changed)
-        menu1 = MMenu(parent=self)
+        page_setting_menu = MMenu(parent=self)
 
         self._display_label = MLabel()
         self._display_label.setAlignment(Qt.AlignCenter)
-        self._change_page_size_button = MComboBox(size=size)
-        self._change_page_size_button.setFixedWidth(100)
-        self._change_page_size_button.set_menu(menu1)
+        self._change_page_size_button = MComboBox().small()
+        self._change_page_size_button.setFixedWidth(110)
+        self._change_page_size_button.set_menu(page_setting_menu)
         self._change_page_size_button.set_formatter(lambda x: u'{} per page'.format(x))
+        self._change_page_size_button.sig_value_changed.connect(self._emit_page_changed)
 
-        self._pre_button = MToolButton(type=MToolButton.IconOnlyType, icon=MIcon('left_line.svg'), size=size)
+        self._pre_button = MToolButton().icon_only().svg('left_fill.svg').small()
         self._pre_button.clicked.connect(functools.partial(self._slot_change_current_page, -1))
-        self._next_button = MToolButton(type=MToolButton.IconOnlyType, icon=MIcon('right_line.svg'), size=size)
+        self._next_button = MToolButton().small().icon_only().svg('right_fill.svg')
         self._next_button.clicked.connect(functools.partial(self._slot_change_current_page, 1))
         self._current_page_spin_box = MSpinBox()
-        self._current_page_spin_box.set_dayu_size(size)
         self._current_page_spin_box.setMinimum(1)
-        # self._current_page_spin_box.valueChanged.connect(self.sig_current_page_changed)
+        self._current_page_spin_box.set_dayu_size(dayu_theme.small)
+        self._current_page_spin_box.valueChanged.connect(self._emit_page_changed)
         self._total_page_label = MLabel()
 
-        self.bind('page_size_list', menu1, 'data')
-        self.bind('page_size_selected', menu1, 'value', signal='sig_value_changed')
-        self.bind('page_size_selected', self._change_page_size_button, 'value', signal='sig_value_changed')
+        self.bind('page_size_list', page_setting_menu, 'data')
+        self.bind('page_size_selected', page_setting_menu, 'value', signal='sig_value_changed')
+        self.bind('page_size_selected', self._change_page_size_button, 'value',
+                  signal='sig_value_changed')
         self.bind('current_page', self._current_page_spin_box, 'value', signal='valueChanged')
         self.bind('total_page', self._current_page_spin_box, 'maximum')
-        self.bind('total_page', self._total_page_label, 'text')
-        self.bind('display_text', self._display_label, 'text')
+        self.bind('total_page_text', self._total_page_label, 'dayu_text')
+        self.bind('display_text', self._display_label, 'dayu_text')
         self.bind('can_pre', self._pre_button, 'enabled')
         self.bind('can_next', self._next_button, 'enabled')
 
@@ -82,9 +87,9 @@ class MPage(QWidget, MFieldMixin):
         main_lay.addStretch()
         main_lay.addWidget(self._display_label)
         main_lay.addStretch()
-        main_lay.addWidget(MLabel.secondary('|'))
+        main_lay.addWidget(MLabel('|').secondary())
         main_lay.addWidget(self._change_page_size_button)
-        main_lay.addWidget(MLabel.secondary('|'))
+        main_lay.addWidget(MLabel('|').secondary())
         main_lay.addWidget(self._pre_button)
         main_lay.addWidget(MLabel('Page'))
         main_lay.addWidget(self._current_page_spin_box)
@@ -94,12 +99,19 @@ class MPage(QWidget, MFieldMixin):
         self.setLayout(main_lay)
 
     def set_total(self, value):
+        """Set page component total count."""
         self.set_field('total', value)
         self.set_field('current_page', 1)
 
     def _slot_change_current_page(self, offset):
         self.set_field('current_page', self.field('current_page') + offset)
+        self._emit_page_changed()
 
     def set_page_config(self, data_list):
+        """Set page component per page settings."""
         self.set_field('page_size_list',
-                       [{'label': str(data), 'value': data} if isinstance(data, int) else data for data in data_list])
+                       [{'label': str(data), 'value': data} if isinstance(data, int) else data for
+                        data in data_list])
+
+    def _emit_page_changed(self):
+        self.sig_page_changed.emit(self.field('page_size_selected'), self.field('current_page'))
