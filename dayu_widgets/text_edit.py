03644efbffcb64ca3360b514b797b182f47460b3
muyanru <muyanru@phenom-films.com>
2019-07-03 19:30:07 +0800

    add basic text edit

---
+++ dayu_widgets/text_edit.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.6
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.qt import QSizeGrip, QTextEdit, QGridLayout, Qt
+
+
+class MSizeGrip(QSizeGrip):
+    def __init__(self, parent=None):
+        super(MSizeGrip, self).__init__(parent)
+
+
+class MTextEdit(QTextEdit):
+    def __init__(self, parent=None):
+        super(MTextEdit, self).__init__(parent)
+        self.setWindowFlags(Qt.SubWindow)
+        self._size_grip = MSizeGrip(self)
+        layout = QGridLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self._size_grip, 0, 0, Qt.AlignBottom | Qt.AlignRight)
+        self.setLayout(layout)
+        self._size_grip.setVisible(False)
+
+    def autosize(self):
+        self.textChanged.connect(self._autosize_text_edit)
+        return self
+
+    def _autosize_text_edit(self):
+        # w = self.width()
+        doc = self.document()
+        print self.width(), doc.lineCount(), doc.idealWidth()
+
+    def resizeable(self):
+        """Show the size grip on bottom right. User can use it to resize MTextEdit"""
+        self._size_grip.setVisible(True)
+        return self
---
+++ examples/text_edit_example.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2019.6
+# Email : muyanru345@163.com
+###################################################################
+
+from dayu_widgets.text_edit import MTextEdit
+from dayu_widgets.label import MLabel
+from dayu_widgets.divider import MDivider
+from dayu_widgets.push_button import MPushButton
+from dayu_widgets.qt import *
+
+
+
+class TextEditExample(QWidget):
+    def __init__(self, parent=None):
+        super(TextEditExample, self).__init__(parent)
+        self._init_ui()
+
+    def _init_ui(self):
+        main_lay = QVBoxLayout()
+
+        main_lay.addWidget(MDivider('no size grip'))
+        main_lay.addWidget(MTextEdit(self))
+        main_lay.addWidget(MDivider('size grip'))
+        main_lay.addWidget(MTextEdit(self).resizeable())
+        main_lay.addWidget(MPushButton('text').primary())
+
+        self.setLayout(main_lay)
+
+if __name__ == '__main__':
+    import sys
+    from dayu_widgets.qt import QApplication
+    from dayu_widgets import dayu_theme
+    app = QApplication(sys.argv)
+    test = TextEditExample()
+    dayu_theme.apply(test)
+    test.show()
+    sys.exit(app.exec_())
