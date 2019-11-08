966fe0152f6f0916d82f3b8b00af969416c7573d
Yanru Mu <muyanru345@163.com>
2019-11-08 20:39:25 +0800

    add theme example to show basic form widgets

--- examples/theme_example.py
+++ examples/theme_example.py
@@ -8,11 +8,17 @@
 
 import functools
 
-from dayu_widgets.button_group import MRadioButtonGroup
 from dayu_widgets.divider import MDivider
 from dayu_widgets.field_mixin import MFieldMixin
 from dayu_widgets.push_button import MPushButton
-from dayu_widgets.qt import *
+from dayu_widgets.spin_box import MSpinBox, MDateEdit
+from dayu_widgets.check_box import MCheckBox
+from dayu_widgets.combo_box import MComboBox
+from dayu_widgets.line_edit import MLineEdit
+from dayu_widgets.button_group import MRadioButtonGroup
+from dayu_widgets.switch import MSwitch
+from dayu_widgets.slider import MSlider
+from dayu_widgets.qt import QWidget, QFormLayout, Qt, MIcon, QHBoxLayout, QVBoxLayout
 
 
 class ThemeExample(QWidget, MFieldMixin):
@@ -21,77 +27,47 @@ class ThemeExample(QWidget, MFieldMixin):
         self._init_ui()
 
     def _init_ui(self):
-        radio_group_h = MRadioButtonGroup()
-        radio_group_h.set_button_list(['Apple', {'text': 'Banana'}, {'text': 'Pear'}])
-        radio_grp_1_lay = QHBoxLayout()
-        radio_grp_1_lay.addWidget(radio_group_h)
-        radio_grp_1_lay.addStretch()
+        form_lay = QFormLayout()
+        form_lay.setLabelAlignment(Qt.AlignRight)
+        gender_grp = MRadioButtonGroup()
+        gender_grp.set_button_list([{'text': 'Female', 'icon': MIcon('female.svg')},
+                                    {'text': 'Male', 'icon': MIcon('male.svg')}])
 
-        color_data = [
-            {'text': 'Maya', 'icon': MIcon('app-maya.png')},
-            {'text': 'Nuke', 'icon': MIcon('app-nuke.png')},
-            {'text': 'Houdini', 'icon': MIcon('app-houdini.png')}
-        ]
+        country_combo_box = MComboBox().small()
+        country_combo_box.addItems(['China', 'France', 'Japan', 'US'])
+        date_edit = MDateEdit().small()
+        date_edit.setCalendarPopup(True)
 
-        radio_group_v = MRadioButtonGroup(orientation=Qt.Vertical)
-        radio_group_v.set_button_list(color_data)
+        form_lay.addRow('Name:', MLineEdit().small())
+        form_lay.addRow('Gender:', gender_grp)
+        form_lay.addRow('Age:', MSpinBox().small())
+        form_lay.addRow('Password:', MLineEdit().small().password())
+        form_lay.addRow('Country:', country_combo_box)
+        form_lay.addRow('Birthday:', date_edit)
+        switch = MSwitch()
+        switch.setChecked(True)
+        form_lay.addRow('Switch:', switch)
+        slider = MSlider()
+        slider.setValue(30)
+        form_lay.addRow('Slider:', slider)
 
-        radio_group_button_h = MRadioButtonGroup()
-        radio_group_button_h.set_button_list(color_data)
-        radio_grp_h_lay = QHBoxLayout()
-        radio_grp_h_lay.addWidget(radio_group_button_h)
-        radio_grp_h_lay.addStretch()
-
-        radio_group_button_v = MRadioButtonGroup(orientation=Qt.Vertical)
-        radio_group_button_v.set_button_list(color_data)
-
-        self.register_field('value1', -1)
-        self.register_field('value1_text', functools.partial(self.value_to_text, 'value1', color_data))
-        self.register_field('value2', 0)
-        self.register_field('value2_text', functools.partial(self.value_to_text, 'value2', color_data))
-        self.register_field('value3', -1)
-        self.register_field('value3_text', functools.partial(self.value_to_text, 'value3', color_data))
-
-        button1 = MPushButton(text='Group 1')
-        button2 = MPushButton(text='Group 2')
-        button3 = MPushButton(text='Group 3')
-        button1.clicked.connect(functools.partial(self.slot_change_value, 'value1'))
-        button2.clicked.connect(functools.partial(self.slot_change_value, 'value2'))
-        button3.clicked.connect(functools.partial(self.slot_change_value, 'value3'))
-
-        self.bind('value1', radio_group_v, 'dayu_checked', signal='sig_checked_changed')
-        self.bind('value2', radio_group_button_h, 'dayu_checked', signal='sig_checked_changed')
-        self.bind('value3', radio_group_button_v, 'dayu_checked', signal='sig_checked_changed')
-        self.bind('value1_text', button1, 'text')
-        self.bind('value2_text', button2, 'text')
-        self.bind('value3_text', button3, 'text')
+        button_lay = QHBoxLayout()
+        button_lay.addStretch()
+        button_lay.addWidget(MPushButton(text='Submit').primary())
+        button_lay.addWidget(MPushButton(text='Cancel'))
 
         main_lay = QVBoxLayout()
-        main_lay.addWidget(MDivider('MRadioButtonGroup: orientation=Qt.Horizontal '))
-        main_lay.addLayout(radio_grp_1_lay)
-        main_lay.addWidget(MDivider('MRadioButtonGroup: orientation=Qt.Vertical'))
-        main_lay.addWidget(radio_group_v)
-        main_lay.addWidget(button1)
-        main_lay.addWidget(MDivider('MRadioButtonGroup: orientation=Qt.Horizontal type=button'))
-        main_lay.addLayout(radio_grp_h_lay)
-        main_lay.addWidget(button2)
-        main_lay.addWidget(MDivider('MRadioButtonGroup: orientation=Qt.Vertical, type=button'))
-        main_lay.addWidget(radio_group_button_v)
-        main_lay.addWidget(button3)
+        main_lay.addLayout(form_lay)
+        main_lay.addWidget(MCheckBox('I accept the terms and conditions'))
         main_lay.addStretch()
+        main_lay.addWidget(MDivider())
+        main_lay.addLayout(button_lay)
         self.setLayout(main_lay)
-
-    def value_to_text(self, field, data_list):
-        return 'Please Select One' if self.field(field) < 0 else \
-            'You Selected [{}]'.format(data_list[self.field(field)].get('text'))
-
-    def slot_change_value(self, attr):
-        import random
-        self.set_field(attr, random.randrange(0, 3))
 
 
 if __name__ == '__main__':
     import sys
+    from dayu_widgets.qt import QApplication
 
     app = QApplication(sys.argv)
     test = ThemeExample()
