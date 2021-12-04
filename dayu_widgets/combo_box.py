7e8ceff6da0c8be1b727254898b55a6c1749ecc8
timmyliang <timmyliang@tencent.com>
2021-12-04 21:14:19 +0800

    fix: clean combox search code

--- dayu_widgets/combo_box.py
+++ dayu_widgets/combo_box.py
@@ -10,34 +10,12 @@ from Qt import QtCore, QtGui, QtWidgets
 import dayu_widgets.utils as utils
 from dayu_widgets import dayu_theme
 from dayu_widgets.mixin import cursor_mixin, focus_shadow_mixin, property_mixin
-from dayu_widgets.qt import (Property, QComboBox, QEvent, QPoint, QSizePolicy,
-                             Signal)
 
 
 @property_mixin
-@cursor_mixin
-@focus_shadow_mixin
-class MComboBox(QComboBox):
-    Separator = "/"
-    sig_value_changed = Signal(object)
-
-    def __init__(self, parent=None):
-        super(MComboBox, self).__init__(parent)
-        self._root_menu = None
-        self._display_formatter = utils.display_formatter
-        self.setEditable(True)
-        line_edit = self.lineEdit()
-        line_edit.setReadOnly(True)
-        line_edit.setTextMargins(4, 0, 4, 0)
-        line_edit.setStyleSheet("background-color:transparent")
-        # line_edit.setCursor(Qt.PointingHandCursor)
-        line_edit.installEventFilter(self)
-        self._has_custom_view = False
-        self.set_value("")
-        self.set_placeholder(self.tr("Please Select"))
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
-        self._dayu_size = dayu_theme.default_size
-
+class MComboBoxSearchMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(MComboBoxSearchMixin, self).__init__(*args, **kwargs)
         self.filter_model = QtCore.QSortFilterProxyModel(self)
         self.filter_model.setFilterCaseSensitivity(QtCore.Qt.CaseInsensitive)
         self.filter_model.setSourceModel(self.model())
@@ -51,7 +29,6 @@ class MComboBox(QComboBox):
 
         popup = self.completer.popup()
         dayu_theme.apply(popup)
-        self.completer.setPopup(popup)
         self.setCompleter(self.completer)
 
         edit = self.lineEdit()
@@ -62,15 +39,47 @@ class MComboBox(QComboBox):
             lambda t: t and self.setCurrentIndex(self.findText(t))
         )
 
+    def _set_search(self, value):
+        """search property to True then trigger search"""
+        value and self.search()
+
     def setModel(self, model):
-        super(MComboBox, self).setModel(model)
+        super(MComboBoxSearchMixin, self).setModel(model)
         self.filter_model.setSourceModel(model)
         self.completer.setModel(self.filter_model)
 
     def setModelColumn(self, column):
         self.completer.setCompletionColumn(column)
         self.filter_model.setFilterKeyColumn(column)
-        super(MComboBox, self).setModelColumn(column)
+        super(MComboBoxSearchMixin, self).setModelColumn(column)
+
+
+@cursor_mixin
+@focus_shadow_mixin
+class MComboBox(MComboBoxSearchMixin, QtWidgets.QComboBox):
+    Separator = "/"
+    sig_value_changed = QtCore.Signal(object)
+
+    def __init__(self, parent=None):
+        super(MComboBox, self).__init__(parent)
+
+        self._root_menu = None
+        self._display_formatter = utils.display_formatter
+        self.setEditable(True)
+        line_edit = self.lineEdit()
+        line_edit.setReadOnly(True)
+        line_edit.setTextMargins(4, 0, 4, 0)
+        line_edit.setStyleSheet("background-color:transparent")
+        line_edit.setCursor(QtCore.Qt.PointingHandCursor)
+        line_edit.installEventFilter(self)
+        self._has_custom_view = False
+        self.set_value("")
+        self.set_placeholder(self.tr("Please Select"))
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum
+        )
+        self._dayu_size = dayu_theme.default_size
+        dayu_theme.apply(self)
 
     def get_dayu_size(self):
         """
@@ -89,7 +98,7 @@ class MComboBox(QComboBox):
         self.lineEdit().setProperty("dayu_size", value)
         self.style().polish(self)
 
-    dayu_size = Property(int, get_dayu_size, set_dayu_size)
+    dayu_size = QtCore.Property(int, get_dayu_size, set_dayu_size)
 
     def set_formatter(self, func):
         self._display_formatter = func
@@ -121,15 +130,15 @@ class MComboBox(QComboBox):
         if self._has_custom_view or self._root_menu is None:
             super(MComboBox, self).showPopup()
         else:
-            QComboBox.hidePopup(self)
-            self._root_menu.popup(self.mapToGlobal(QPoint(0, self.height())))
+            super(MComboBox, self).hidePopup()
+            self._root_menu.popup(self.mapToGlobal(QtCore.QPoint(0, self.height())))
 
     # def setCurrentIndex(self, index):
     #     raise NotImplementedError
 
     def eventFilter(self, widget, event):
         if widget is self.lineEdit() and widget.isReadOnly():
-            if event.type() == QEvent.MouseButtonPress:
+            if event.type() == QtCore.QEvent.MouseButtonPress:
                 self.showPopup()
         return super(MComboBox, self).eventFilter(widget, event)
 
